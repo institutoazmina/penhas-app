e3547407d2ae672f24fa877b74fff9990c421f09
Solli Honorio <shonorio@gmail.com>
2020-04-15 20:58:48 +0200

    parsing success login json

--- penhas/lib/app/modules/penhas/services/authentication_service.dart
+++ penhas/lib/app/modules/penhas/services/authentication_service.dart
@@ -1,7 +1,35 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
 class PenhasAuthenticationService extends Disposable {
-  //dispose will be called automatically
+  final IApiProvider apiClient;
+
+  PenhasAuthenticationService({this.apiClient});
+
+  Future<AuthenticationModel> login({String email, String password}) async {
+    return apiClient.request(_LoginRequest()).then(_mapLoginModel);
+  }
+
+  Future<AuthenticationModel> _mapLoginModel(Map<String, dynamic> data) async {
+    var fakePassword = data['senha_falsa'] > 0;
+    return AuthenticationModel(
+        fakePassword: fakePassword, sessionToken: data['session']);
+  }
+
   @override
   void dispose() {}
 }
+
+class _LoginRequest extends IRequestBuilder {}
+
+class AuthenticationModel {
+  final bool fakePassword;
+  final String sessionToken;
+
+  AuthenticationModel({this.fakePassword, this.sessionToken});
+}
+
+abstract class IApiProvider {
+  Future<Map<String, dynamic>> request(IRequestBuilder requestBuilder);
+}
+
+abstract class IRequestBuilder {}
--- penhas/test/app/modules/penhas/services/authentication_service_test.dart
+++ penhas/test/app/modules/penhas/services/authentication_service_test.dart
@@ -1,5 +1,5 @@
-import 'dart:io';
-
+import 'dart:io' show File;
+import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:penhas/app/modules/penhas/services/authentication_service.dart';
@@ -10,17 +10,33 @@ void main() {
 
   setUp(() {
     httpClient = _HttpClientSpy();
-    sut = PenhasAuthenticationService();
+    sut = PenhasAuthenticationService(apiClient: httpClient);
   });
 
   group('AuthenticationService Test', () {
-    test("Initialization do not call request", () {
+    test("do not call request on initialization", () async {
       expect(sut, isInstanceOf<PenhasAuthenticationService>());
       expect(httpClient.requestMessages, []);
+    });
+
+    test("success login with valid parameters", () async {
+      var response =
+          await sut.login(email: "user@valid.com", password: 'strong_password');
+      expect(response, isInstanceOf<AuthenticationModel>());
+      expect(response.fakePassword, false);
+      expect(response.sessionToken, 'my_strong_password');
     });
   });
 }
 
-class _HttpClientSpy {
+class _HttpClientSpy extends IApiProvider {
   var requestMessages = [];
+
+  @override
+  Future<Map<String, dynamic>> request(IRequestBuilder requestBuilder) async {
+    final file = File('test/assets/json/penhas_login_success.json');
+    final content = await file.readAsString();
+    final data = JsonDecoder().convert(content);
+    return data;
+  }
 }
