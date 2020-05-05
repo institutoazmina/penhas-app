35a047315575dee82551f64b87d73c67d08cd93a
Solli Honorio <shonorio@gmail.com>
2020-05-05 18:37:35 +0200

    configured network objects for dependency injection

--- penhas/lib/app/app_module.dart
+++ penhas/lib/app/app_module.dart
@@ -1,7 +1,10 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:penhas/app/core/network/api_server_configure.dart';
+import 'package:penhas/app/core/network/network_info.dart';
 import 'package:penhas/app/features/authentication/presentation/sign_in/sign_in_module.dart';
-import 'package:penhas/app/modules/authentication/authentication_module.dart';
 import 'package:penhas/app/modules/splash/splash_controller.dart';
 import 'package:penhas/app/modules/splash/splash_module.dart';
 
@@ -9,11 +12,18 @@ import 'app_controller.dart';
 import 'app_widget.dart';
 
 class AppModule extends MainModule {
+  final Uri apiServer = Uri.https('elasv2-api.appcivico.com', '/');
   @override
   List<Bind> get binds => [
         Bind((i) => AppController()),
         Bind((i) => SplashController()),
         Bind((i) => SignInModule()),
+        Bind<IApiServerConfigure>(
+          (i) => ApiServerConfigure(baseUri: apiServer),
+        ),
+        Bind<http.Client>((i) => http.Client()),
+        Bind<INetworkInfo>((i) => NetworkInfo(i.get<DataConnectionChecker>())),
+        Bind((i) => DataConnectionChecker()),
       ];
 
   @override
@@ -26,40 +36,4 @@ class AppModule extends MainModule {
   Widget get bootstrap => AppWidget();
 
   static Inject get to => Inject<AppModule>.of();
-
-  // Widget _buildAppBar() {
-  //   return AppBar(
-  //     elevation: 0,
-  //     title: Row(
-  //       children: <Widget>[Icon(DesignSystemLogo.penhasLogo)],
-  //     ),
-  //     leading: GestureDetector(
-  //       onTap: controller.onMenuAction,
-  //       child: Icon(Icons.menu),
-  //     ),
-  //     actions: <Widget>[
-  //       Padding(
-  //         padding: EdgeInsets.only(right: 20.0),
-  //         child: GestureDetector(
-  //           onTap: controller.onSearchAction,
-  //           child: Icon(
-  //             Icons.search,
-  //             size: 26.0,
-  //           ),
-  //         ),
-  //       ),
-  //       Padding(
-  //         padding: EdgeInsets.only(right: 20.0),
-  //         child: GestureDetector(
-  //           onTap: controller.onNotificationAction,
-  //           child: Icon(
-  //             Icons.notifications,
-  //             size: 26.0,
-  //           ),
-  //         ),
-  //       )
-  //     ],
-  //     backgroundColor: DesignSystemColors.LigthPurple,
-  //   );
-  // }
 }
