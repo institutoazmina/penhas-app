fafd574cd9e978a0eac5fa8364bb82266b5a7c7e
Solli Honorio <shonorio@gmail.com>
2020-05-21 13:50:48 +0200

    included new assets

--- penhas/lib/app/app_widget.dart
+++ penhas/lib/app/app_widget.dart
@@ -8,6 +8,8 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Penhas',
+      theme: ThemeData(
+          textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Lato')),
       navigatorKey: Modular.navigatorKey,
       localizationsDelegates: [
         GlobalWidgetsLocalizations.delegate,
