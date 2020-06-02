ab75cbaec0c215dea827e5559aef1f7b17c53d64
Solli Honorio <shonorio@gmail.com>
2020-06-03 00:18:34 +0200

    TweetRepository should retrieve tweets from a valid session

---
+++ penhas/lib/app/features/feed/data/repositories/tweet_repository.dart
@@ -0,0 +1,60 @@
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import 'package:penhas/app/core/error/exceptions.dart';
+import 'package:penhas/app/core/error/failures.dart';
+import 'package:penhas/app/core/network/network_info.dart';
+import 'package:penhas/app/features/feed/data/models/tweet_session_model.dart';
+import 'package:penhas/app/features/feed/domain/entities/tweet_session_entity.dart';
+import 'package:penhas/app/features/feed/domain/repositories/i_tweet_repositories.dart';
+
+abstract class ITweetDataSource {
+  Future<TweetSessionMondel> retrieve();
+}
+
+abstract class ITweetRepository {
+  Future<Either<Failure, TweetSessionEntity>> retrieve();
+}
+
+class TweetRepository implements ITweetRepository {
+  final INetworkInfo _networkInfo;
+  final ITweetDataSource _dataSource;
+
+  TweetRepository({
+    @required INetworkInfo networkInfo,
+    @required ITweetDataSource dataSource,
+  })  : this._dataSource = dataSource,
+        this._networkInfo = networkInfo;
+
+  @override
+  Future<Either<Failure, TweetSessionEntity>> retrieve() async {
+    try {
+      return right(await _dataSource.retrieve());
+    } catch (e) {
+      return left(await _handleError(e));
+    }
+  }
+
+  Future<Failure> _handleError(Object error) async {
+    if (await _networkInfo.isConnected == false) {
+      return InternetConnectionFailure();
+    }
+
+    if (error is ApiProviderException) {
+      if (error.bodyContent['error'] == 'expired_jwt') {
+        return ServerSideSessionFailed();
+      }
+
+      return ServerSideFormFieldValidationFailure(
+          error: error.bodyContent['error'],
+          field: error.bodyContent['field'],
+          reason: error.bodyContent['reason'],
+          message: error.bodyContent['message']);
+    }
+
+    if (error is ApiProviderSessionExpection) {
+      return ServerSideSessionFailed();
+    }
+
+    return ServerFailure();
+  }
+}
---
+++ penhas/test/app/features/feed/data/repositories/feed_repository_test.dart
@@ -0,0 +1,66 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:penhas/app/core/network/network_info.dart';
+import 'package:penhas/app/features/feed/data/models/tweet_session_model.dart';
+import 'package:penhas/app/features/feed/data/repositories/tweet_repository.dart';
+import 'package:penhas/app/features/feed/domain/entities/tweet_session_entity.dart';
+
+import '../../../../../utils/json_util.dart';
+
+class MockNetworkInfo extends Mock implements INetworkInfo {}
+
+class MockTweetDataSource extends Mock implements ITweetDataSource {}
+
+void main() {
+  ITweetRepository repository;
+  TweetSessionMondel sessionMondel;
+  INetworkInfo networkInfo;
+  ITweetDataSource dataSource;
+  Map<String, Object> jsonSession;
+
+  setUp(() async {
+    networkInfo = MockNetworkInfo();
+    dataSource = MockTweetDataSource();
+    repository =
+        TweetRepository(dataSource: dataSource, networkInfo: networkInfo);
+    jsonSession = await JsonUtil.getJson(from: 'feed/retrieve_response.json');
+    sessionMondel = TweetSessionMondel.fromJson(jsonSession);
+    when(dataSource.retrieve()).thenAnswer((_) => Future.value(sessionMondel));
+  });
+
+  group('TweetRepository', () {
+    test('should retrieve tweets from a valid session', () async {
+      // arrange
+      final TweetSessionEntity expectedSession = sessionMondel;
+      // act
+      final receivedSession = await repository.retrieve();
+      // assert
+      expect(receivedSession, right(expectedSession));
+    });
+
+    test('should create tweet from a valid session', () async {
+      // arrange
+      // act
+      // assert
+    });
+
+    test('should favorite a valid tweet', () async {
+      // arrange
+      // act
+      // assert
+    });
+
+    test('should reply a valid tweet', () async {
+      // arrange
+      // act
+      // assert
+    });
+
+    test('should report a valid tweet', () async {
+      // arrange
+      // act
+      // assert
+    });
+  });
+}
