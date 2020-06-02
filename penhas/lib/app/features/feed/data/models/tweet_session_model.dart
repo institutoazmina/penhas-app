08e99ae9c9a125bf16b7bbc38d6de57a95c478e4
Solli Honorio <shonorio@gmail.com>
2020-06-02 23:31:13 +0200

    TweetResponseModel should return a valid model with a valid JSON

--- penhas/lib/app/features/feed/data/models/tweet_session_model.dart
+++ penhas/lib/app/features/feed/data/models/tweet_session_model.dart
@@ -8,4 +8,46 @@ class TweetSessionMondel extends TweetSessionEntity {
 
   TweetSessionMondel(this.hasMore, this.orderBy, this.tweets)
       : super(hasMore: hasMore, orderBy: orderBy, tweets: tweets);
+
+  factory TweetSessionMondel.fromJson(Map<String, Object> jsonData) {
+    final hasMore = jsonData['has_more'] == 1 ?? false;
+    final orderBy = jsonData['order_by'] == 'latest_first'
+        ? TweetSessionOrder.latestFirst
+        : TweetSessionOrder.oldestFirst;
+    final tweets = _parseTweet(jsonData['tweets']);
+
+    return TweetSessionMondel(hasMore, orderBy, tweets);
+  }
+
+  static List<TweetEntity> _parseTweet(List<Object> tweets) {
+    if (tweets == null || tweets.isEmpty) {
+      return [];
+    }
+
+    return tweets
+        .map((e) => e as Map<String, Object>)
+        .map((e) => _mapToTweet(e))
+        .toList();
+  }
+
+  static TweetEntity _mapToTweet(Map<String, Object> data) {
+    final meta = data['meta'] as Map<String, Object>;
+    final tweetMeta = TweetMeta(
+      liked: meta['liked'] == 1 ?? false,
+      owner: meta['owner'] == 1 ?? false,
+    );
+
+    return TweetEntity(
+      id: data['id'],
+      userName: data['name'],
+      createdAt: data['created_at'],
+      clientId: (data['cliente_id'] as num).toInt(),
+      totalLikes: (data['qtde_likes'] as num).toInt(),
+      totalReply: (data['qtde_comentarios'] as num).toInt(),
+      anonymous: data['anonimo'] != 0,
+      content: data['content'],
+      avatar: data['icon'],
+      meta: tweetMeta,
+    );
+  }
 }
--- penhas/test/app/features/feed/data/models/tweet_response_model_test.dart
+++ penhas/test/app/features/feed/data/models/tweet_response_model_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:penhas/app/features/feed/data/models/tweet_session_model.dart';
+import 'package:penhas/app/features/feed/domain/entities/tweet_entity.dart';
 import 'package:penhas/app/features/feed/domain/entities/tweet_session_entity.dart';
+
+import '../../../../../utils/json_util.dart';
 
 void main() {
   TweetSessionMondel tweetSessionMondel;
@@ -16,6 +19,43 @@ void main() {
       );
       // assert
       expect(tweetSessionMondel, isA<TweetSessionEntity>());
+    });
+
+    test('should return a valid model with a valid JSON', () async {
+      // arrange
+      final jsonData =
+          await JsonUtil.getJson(from: 'feed/retrieve_response.json');
+      final TweetSessionEntity expectedSession =
+          TweetSessionMondel(true, TweetSessionOrder.latestFirst, [
+        TweetEntity(
+          id: '200528T2055380001',
+          userName: 'penhas',
+          clientId: 551,
+          createdAt: '2020-05-28 20:55:38',
+          totalReply: 0,
+          totalLikes: 0,
+          anonymous: false,
+          content: 'sleep 7',
+          avatar: 'https:\/\/elasv2-api.appcivico.com\/avatar\/padrao.svg',
+          meta: TweetMeta(liked: false, owner: true),
+        ),
+        TweetEntity(
+          id: '200528T2055370004',
+          userName: 'penhas',
+          clientId: 551,
+          createdAt: '2020-05-28 20:55:37',
+          totalReply: 0,
+          totalLikes: 0,
+          anonymous: false,
+          content: 'sleep 6',
+          avatar: 'https:\/\/elasv2-api.appcivico.com\/avatar\/padrao.svg',
+          meta: TweetMeta(liked: false, owner: true),
+        ),
+      ]);
+      // act
+      final result = TweetSessionMondel.fromJson(jsonData);
+      // assert
+      expect(expectedSession, result);
     });
   });
 }
