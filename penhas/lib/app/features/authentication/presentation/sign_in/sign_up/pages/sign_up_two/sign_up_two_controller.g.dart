aa60a18ce56272ec728ee7999f9c90ffc51d82b8
Solli Honorio <shonorio@gmail.com>
2020-06-24 09:02:10 +0200

    updated auto generated files

--- penhas/lib/app/features/authentication/presentation/sign_in/sign_up/pages/sign_up_two/sign_up_two_controller.g.dart
+++ penhas/lib/app/features/authentication/presentation/sign_in/sign_up/pages/sign_up_two/sign_up_two_controller.g.dart
@@ -6,7 +6,7 @@ part of 'sign_up_two_controller.dart';
 // StoreGenerator
 // **************************************************************************
 
-// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$SignUpTwoController on _SignUpTwoControllerBase, Store {
   Computed<PageProgressState> _$currentStateComputed;
