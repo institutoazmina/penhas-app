04de06f2b77c0f17de3e3551d6021f0660a0fd8e
Solli Honorio <shonorio@gmail.com>
2020-06-29 13:36:00 +0200

    TweetFilterPreferenceRepository fetch() should retrieve tweets from a valid session

--- penhas/lib/app/features/feed/data/repositories/tweet_filter_preference_repository.dart
+++ penhas/lib/app/features/feed/data/repositories/tweet_filter_preference_repository.dart
@@ -1,7 +1,57 @@
 import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import 'package:penhas/app/core/error/exceptions.dart';
 import 'package:penhas/app/core/error/failures.dart';
+import 'package:penhas/app/core/network/network_info.dart';
+import 'package:penhas/app/features/feed/data/datasources/tweet_filter_preference_data_source.dart';
 import 'package:penhas/app/features/feed/domain/entities/tweet_filter_session_entity.dart';
 
 abstract class ITweetFilterPreferenceRepository {
   Future<Either<Failure, TweetFilterSessionEntity>> retreive();
+}
+
+class TweetFilterPreferenceRepository
+    implements ITweetFilterPreferenceRepository {
+  final INetworkInfo _networkInfo;
+  final ITweetFilterPreferenceDataSource _dataSource;
+
+  TweetFilterPreferenceRepository({
+    @required INetworkInfo networkInfo,
+    @required ITweetFilterPreferenceDataSource dataSource,
+  })  : this._dataSource = dataSource,
+        this._networkInfo = networkInfo;
+
+  @override
+  Future<Either<Failure, TweetFilterSessionEntity>> retreive() async {
+    try {
+      final result = await _dataSource.fetch();
+      return right(result);
+    } catch (e) {
+      return left(await _handleError(e));
+    }
+  }
+
+  Future<Failure> _handleError(Object error) async {
+    if (await _networkInfo.isConnected == false) {
+      return InternetConnectionFailure();
+    }
+
+    if (error is ApiProviderException) {
+      if (error.bodyContent['error'] == 'expired_jwt') {
+        return ServerSideSessionFailed();
+      }
+
+      return ServerSideFormFieldValidationFailure(
+          error: error.bodyContent['error'],
+          field: error.bodyContent['field'],
+          reason: error.bodyContent['reason'],
+          message: error.bodyContent['message']);
+    }
+
+    if (error is ApiProviderSessionExpection) {
+      return ServerSideSessionFailed();
+    }
+
+    return ServerFailure();
+  }
 }
--- penhas/test/app/features/feed/data/repositories/tweet_filter_repository_test.dart
+++ penhas/test/app/features/feed/data/repositories/tweet_filter_repository_test.dart
@@ -1,14 +1,54 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:penhas/app/core/network/network_info.dart';
+import 'package:penhas/app/features/feed/data/datasources/tweet_filter_preference_data_source.dart';
+import 'package:penhas/app/features/feed/data/models/tweet_filter_session_model.dart';
 import 'package:penhas/app/features/feed/data/repositories/tweet_filter_preference_repository.dart';
+import 'package:penhas/app/features/feed/domain/entities/tweet_filter_session_entity.dart';
+
+import '../../../../../utils/json_util.dart';
 
 class MockTweetFilterRepository extends Mock
     implements ITweetFilterPreferenceRepository {}
 
+class MockNetworkInfo extends Mock implements INetworkInfo {}
+
+class MockTweetFilterPreferenceDataSource extends Mock
+    implements ITweetFilterPreferenceDataSource {}
+
 void main() {
+  INetworkInfo networkInfo;
   ITweetFilterPreferenceRepository sut;
+  ITweetFilterPreferenceDataSource dataSource;
+  Map<String, Object> jsonSession;
 
-  setUp(() {});
+  setUp(() async {
+    networkInfo = MockNetworkInfo();
+    dataSource = MockTweetFilterPreferenceDataSource();
+    sut = TweetFilterPreferenceRepository(
+      networkInfo: networkInfo,
+      dataSource: dataSource,
+    );
+  });
 
-  group('', () {});
+  group('TweetFilterPreferenceRepository', () {
+    TweetFilterSessionModel sessionModel;
+    setUp(() async {
+      jsonSession =
+          await JsonUtil.getJson(from: 'feed/retrieve_fiters_tags.json');
+      sessionModel = TweetFilterSessionModel.fromJson(jsonSession);
+      when(dataSource.fetch()).thenAnswer((_) => Future.value(sessionModel));
+    });
+    group('fetch()', () {
+      test('should retrieve tweets from a valid session', () async {
+        // arrange
+        final TweetFilterSessionEntity expectedSession = sessionModel;
+        // act
+        final receivedSession = await sut.retreive();
+        // assert
+        expect(receivedSession, right(expectedSession));
+      });
+    });
+  });
 }
