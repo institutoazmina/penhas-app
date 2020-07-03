c73b8bdb0f52d62e4f9d3d9836ec4acf6274f02d
Solli Honorio <shonorio@gmail.com>
2020-07-03 12:01:31 +0200

    Expand is causing tilt on listview, I removed it

--- penhas/lib/app/features/feed/presentation/tweet/widgets/tweet_body.dart
+++ penhas/lib/app/features/feed/presentation/tweet/widgets/tweet_body.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:penhas/app/shared/design_system/text_styles.dart';
 
-import 'helpers/expand_child.dart';
-
 class TweetBody extends StatelessWidget {
   final String bodyContent;
   const TweetBody({
@@ -22,14 +20,7 @@ class TweetBody extends StatelessWidget {
 
     return Padding(
       padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 12.0),
-      child: ExpandChild(
-        expandedHint: 'menos',
-        collapsedHint: 'mais',
-        alignment: Alignment.topLeft,
-        expandArrowStyle: ExpandArrowStyle.both,
-        trimSize: 100.0,
-        child: htmlBody,
-      ),
+      child: htmlBody,
     );
   }
 }
