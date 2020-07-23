c040f0f935e1e728ad3c5b97a592a79bc3e184cb
Solli Honorio <shonorio@gmail.com>
2020-07-23 10:59:27 +0100

    removed dead code

--- penhas/test/app/modules/authentication/authentication_page_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-main() {
-  testWidgets('AuthenticationPage has title', (WidgetTester tester) async {
-    //  await tester.pumpWidget(buildTestableWidget(AuthenticationPage(title: 'Authentication')));
-    //  final titleFinder = find.text('Authentication');
-    //  expect(titleFinder, findsOneWidget);
-  });
-}
