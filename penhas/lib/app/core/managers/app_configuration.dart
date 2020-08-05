26c39e665b0fdf0c23e1c285e58b8f74255de831
Solli Honorio <shonorio@gmail.com>
2020-08-05 17:49:56 +0100

    store app mode

--- penhas/lib/app/core/managers/app_configuration.dart
+++ penhas/lib/app/core/managers/app_configuration.dart
@@ -1,17 +1,23 @@
+import 'dart:convert';
+
 import 'package:meta/meta.dart';
 import 'package:penhas/app/core/data/authorization_status.dart';
 import 'package:penhas/app/core/storage/i_local_storage.dart';
+import 'package:penhas/app/features/appstate/domain/entities/app_state_entity.dart';
 
 abstract class IAppConfiguration {
   Future<String> get apiToken;
   Future<void> saveApiToken({@required String token});
+  Future<void> saveAppModes(AppStateModeEntity appMode);
   Future<void> logout();
   Uri get penhasServer;
   Future<AuthorizationStatus> get authorizationStatus;
+  Future<AppStateModeEntity> get appMode;
 }
 
 class AppConfiguration implements IAppConfiguration {
   final _tokenKey = 'br.com.penhas.tokenServer';
+  final _appModes = 'br.com.penhas.appConfigurationModes';
   final ILocalStorage _storage;
 
   AppConfiguration({@required ILocalStorage storage}) : this._storage = storage;
@@ -42,5 +48,30 @@ class AppConfiguration implements IAppConfiguration {
   Future<void> logout() async {
     await _storage.delete(_tokenKey);
     return;
+  }
+
+  @override
+  Future<void> saveAppModes(AppStateModeEntity appMode) async {
+    final value = jsonEncode(appMode.toJson());
+    await _storage.put(_appModes, value);
+    return;
+  }
+
+  @override
+  Future<AppStateModeEntity> get appMode async {
+    return _storage
+        .get(_appModes)
+        .then((source) => jsonDecode(source))
+        .then((v) => v as Map<String, Object>)
+        .then((v) => _buildAppStateMode(v))
+        .catchError((_) => null);
+  }
+
+  AppStateModeEntity _buildAppStateMode(Map<String, Object> data) {
+    return AppStateModeEntity(
+      stealthModeEnabled: data['stealthModeEnabled'],
+      hasActivedGuardian: data['hasActivedGuardian'],
+      anonymousModeEnabled: data['anonymousModeEnabled'],
+    );
   }
 }
