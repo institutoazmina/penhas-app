25986248668e2c3048e963e2614313fd49498cdb
Solli Honorio <shonorio@gmail.com>
2020-09-27 23:46:55 +0100

    ações de ativar e desativar a gravação

--- penhas/lib/app/features/zodiac/presentation/zodiac_controller.dart
+++ penhas/lib/app/features/zodiac/presentation/zodiac_controller.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:meta/meta.dart';
 import 'package:mobx/mobx.dart';
 import 'package:penhas/app/core/managers/user_profile_store.dart';
 import 'package:penhas/app/features/zodiac/domain/entities/izodiac.dart';
 import 'package:penhas/app/features/zodiac/domain/entities/zodiac_sign_aquarius.dart';
+import 'package:penhas/app/features/zodiac/domain/usecases/stealth_security_action.dart';
 import 'package:penhas/app/features/zodiac/domain/usecases/zodiac.dart';
 
 part 'zodiac_controller.g.dart';
@@ -11,13 +14,21 @@ part 'zodiac_controller.g.dart';
 class ZodiacController extends _ZodiacControllerBase with _$ZodiacController {
   ZodiacController({
     @required IUserProfileStore userProfileStore,
-  }) : super(userProfileStore);
+    @required StealthSecurityAction securityAction,
+  }) : super(userProfileStore, securityAction);
 }
 
 abstract class _ZodiacControllerBase with Store {
   final IUserProfileStore _userProfileStore;
+  final StealthSecurityAction _securityAction;
 
-  _ZodiacControllerBase(this._userProfileStore) {
+  bool _isSecurityRunning = false;
+  StreamSubscription _streamCache;
+
+  _ZodiacControllerBase(
+    this._userProfileStore,
+    this._securityAction,
+  ) {
     _init();
   }
 
@@ -27,6 +38,8 @@ abstract class _ZodiacControllerBase with Store {
     sign = zodiac.sign(_userProfile.birthdate);
     signList =
         zodiac.pickEigthRandonSign(_userProfile.birthdate).asObservable();
+
+    _registerDataSource();
   }
 
   @observable
@@ -42,6 +55,30 @@ abstract class _ZodiacControllerBase with Store {
 
   @action
   void stealthAction() {
-    print('stealthAction');
+    if (_isSecurityRunning) {
+      _securityAction.stop();
+    } else {
+      _securityAction.start();
+    }
+
+    _isSecurityRunning = !_isSecurityRunning;
+  }
+
+  @action
+  void dispose() {
+    _cancelDataSource();
+  }
+
+  _registerDataSource() {
+    _streamCache = _securityAction.isRunning.listen((event) {
+      print(event);
+    });
+  }
+
+  _cancelDataSource() {
+    if (_streamCache != null) {
+      _streamCache.cancel();
+      _streamCache = null;
+    }
   }
 }
