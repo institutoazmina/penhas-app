b4fba0d44c40201692d5e20f4e84caa420910c77
Solli Honorio <shonorio@gmail.com>
2020-10-02 22:29:18 +0100

    trabalhando nos testes do FilterUseCase

--- penhas/lib/app/features/filters/domain/usecases/filter_use_case.dart
+++ penhas/lib/app/features/filters/domain/usecases/filter_use_case.dart
@@ -1,14 +1,11 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:penhas/app/features/filters/data/models/filter_skills_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/features/filters/data/models/filter_tag_model.dart';
+import 'package:penhas/app/features/filters/domain/entities/filter_tag_entity.dart';
 
-import '../../../../../utils/json_util.dart';
-
-void main() {
-  List<FilterTagModel> filterTags;
-
-  setUp(() {
-    filterTags = [
+class FilterUseCase {
+  Future<Either<Failure, List<FilterTagEntity>>> skills() async {
+    return right([
       FilterTagModel(id: "1", label: "Escuta acolhedora", isSelected: false),
       FilterTagModel(id: "2", label: "Psicologia", isSelected: false),
       FilterTagModel(id: "3", label: "Abrigo", isSelected: false),
@@ -17,19 +14,6 @@ void main() {
       FilterTagModel(id: "6", label: "Saúde e bem estar", isSelected: false),
       FilterTagModel(id: "8", label: "Segurança digital", isSelected: false),
       FilterTagModel(id: "9", label: "Segurança pessoal", isSelected: false),
-    ];
-  });
-
-  group('FilterSkillsModel', () {
-    test('should return a valid model with a valid JSON', () async {
-      // arrange
-      final jsonData =
-          await JsonUtil.getJson(from: 'filters/filter_skills.json');
-      final actual = FilterSkillsModel(skills: filterTags);
-      // act
-      final matcher = FilterSkillsModel.fromJson(jsonData);
-      // assert
-      expect(actual, matcher);
-    });
-  });
+    ]);
+  }
 }
--- penhas/test/app/features/filter/domain/usecases/filter_skill_test.dart
+++ penhas/test/app/features/filter/domain/usecases/filter_skill_test.dart
@@ -1,7 +1,34 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:penhas/app/features/filters/domain/entities/filter_tag_entity.dart';
+import 'package:penhas/app/features/filters/domain/usecases/filter_use_case.dart';
 
 void main() {
-  setUp(() {});
+  FilterUseCase sut;
+  List<FilterTagEntity> filterTags;
+
+  setUp(() {
+    filterTags = [
+      FilterTagEntity(id: "1", label: "Escuta acolhedora", isSelected: false),
+      FilterTagEntity(id: "2", label: "Psicologia", isSelected: false),
+      FilterTagEntity(id: "3", label: "Abrigo", isSelected: false),
+      FilterTagEntity(id: "4", label: "Apoio jurídico", isSelected: false),
+      FilterTagEntity(id: "5", label: "Finanças pessoais", isSelected: false),
+      FilterTagEntity(id: "6", label: "Saúde e bem estar", isSelected: false),
+      FilterTagEntity(id: "8", label: "Segurança digital", isSelected: false),
+      FilterTagEntity(id: "9", label: "Segurança pessoal", isSelected: false),
+    ];
+  });
+
+  group('', () {
+    test('should retrieve skills from server', () async {
+      // arrange
+      final actual = filterTags;
+      // act
+      final matcher = await sut.skills();
+      // assert
+      expect(actual, matcher);
+    });
+  });
 }
