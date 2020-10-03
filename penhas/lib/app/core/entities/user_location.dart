c04342c84735153ec1fdcc6e118ab8372755bab9
Solli Honorio <shonorio@gmail.com>
2020-10-03 09:33:01 +0100

    arrumando código após atualização da biblioteca

--- penhas/lib/app/core/entities/user_location.dart
+++ penhas/lib/app/core/entities/user_location.dart
@@ -3,13 +3,14 @@ import 'package:equatable/equatable.dart';
 class UserLocationEntity extends Equatable {
   final double latitude;
   final double longitude;
-  const UserLocationEntity({this.latitude, this.longitude});
+  final double accuracy;
+  const UserLocationEntity({this.latitude, this.longitude, this.accuracy});
 
   @override
-  List<Object> get props => [latitude, longitude];
+  List<Object> get props => [latitude, longitude, accuracy];
 
   @override
   String toString() {
-    return 'UserLocation(latitude: ${latitude.toString()}, longitude: ${longitude.toString()})';
+    return 'UserLocation(latitude: ${latitude.toString()}, longitude: ${longitude.toString()}, accuracy: ${accuracy.toString()})';
   }
 }
