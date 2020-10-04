59cd216b18a5ecb14b1a2773ebca666947a78c21
Solli Honorio <shonorio@gmail.com>
2020-10-04 09:56:59 +0100

    faz pesquisa de usuários utilizando os filtros

--- penhas/lib/app/features/users/data/repositories/users_repository.dart
+++ penhas/lib/app/features/users/data/repositories/users_repository.dart
@@ -6,12 +6,18 @@ import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/network/api_client.dart';
 import 'package:penhas/app/features/authentication/presentation/shared/map_exception_to_failure.dart';
 import 'package:penhas/app/features/users/data/models/user_detail_model.dart';
+import 'package:penhas/app/features/users/data/models/user_search_session_model.dart';
 import 'package:penhas/app/features/users/domain/entities/user_detail_entity.dart';
 import 'package:penhas/app/features/users/domain/entities/user_detail_profile_entity.dart';
+import 'package:penhas/app/features/users/domain/entities/user_search_options.dart';
+import 'package:penhas/app/features/users/domain/entities/user_search_session_entity.dart';
 
 abstract class IUsersRepository {
   Future<Either<Failure, UserDetailEntity>> profileDetail(
     UserDetailProfileEntity profile,
+  );
+  Future<Either<Failure, UserSearchSessionEntity>> search(
+    UserSearchOptions option,
   );
 }
 
@@ -38,6 +44,27 @@ class UsersRepository implements IUsersRepository {
       return left(MapExceptionToFailure.map(error));
     }
   }
+
+  @override
+  Future<Either<Failure, UserSearchSessionEntity>> search(
+      UserSearchOptions option) async {
+    final endPoint = "/search-users";
+    final parameters = {
+      "name": option?.name,
+      "skills": option?.skills?.join(","),
+      "rows": option?.rows?.toString() ?? "100",
+      "next_page": option.nextPage,
+    };
+
+    try {
+      final response = await _apiProvider
+          .get(path: endPoint, parameters: parameters)
+          .parseSearchSession();
+      return right(response);
+    } catch (error) {
+      return left(MapExceptionToFailure.map(error));
+    }
+  }
 }
 
 extension _FutureExtension<T extends String> on Future<T> {
@@ -45,6 +72,13 @@ extension _FutureExtension<T extends String> on Future<T> {
     return this.then((data) async {
       final jsonData = jsonDecode(data) as Map<String, Object>;
       return UserDetailModel.fromJson(jsonData);
+    });
+  }
+
+  Future<UserSearchSessionEntity> parseSearchSession() async {
+    return this.then((data) async {
+      final jsonData = jsonDecode(data) as Map<String, Object>;
+      return UserSearchSessionModel.fromJson(jsonData);
     });
   }
 }
---
+++ penhas/test/app/features/users/data/repositories/users_repository_search_test.dart
@@ -0,0 +1,102 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:penhas/app/core/network/api_client.dart';
+import 'package:penhas/app/features/users/data/models/user_search_session_model.dart';
+import 'package:penhas/app/features/users/data/repositories/users_repository.dart';
+import 'package:penhas/app/features/users/domain/entities/user_search_options.dart';
+
+import '../../../../../utils/json_util.dart';
+
+class MockApiProvider extends Mock implements IApiProvider {}
+
+void main() {
+  String jsonFile;
+  IApiProvider apiProvider;
+  IUsersRepository sut;
+
+  setUp(() {
+    jsonFile = 'users/users_search.json';
+    apiProvider = MockApiProvider();
+    sut = UsersRepository(apiProvider: apiProvider);
+  });
+
+  group('UsersRepository', () {
+    test(
+        'should inform at least rows parameter to server for empty UserSearchOptions ',
+        () async {
+      // arrange
+      final options = UserSearchOptions();
+      when(apiProvider.get(
+        path: anyNamed('path'),
+        headers: anyNamed('headers'),
+        parameters: anyNamed('parameters'),
+      )).thenAnswer((_) => JsonUtil.getString(from: jsonFile));
+      // act
+      await sut.search(options);
+      // assert
+      verify(apiProvider.get(
+        path: "/search-users",
+        parameters: {
+          "name": null,
+          "skills": null,
+          "rows": "100",
+          "next_page": null
+        },
+      ));
+    });
+    test('should inform skill as appended by "," as parameter to server',
+        () async {
+      // arrange
+      final options = UserSearchOptions(skills: ["a", "b", "c"]);
+      when(apiProvider.get(
+        path: anyNamed('path'),
+        headers: anyNamed('headers'),
+        parameters: anyNamed('parameters'),
+      )).thenAnswer((_) => JsonUtil.getString(from: jsonFile));
+      // act
+      await sut.search(options);
+      // assert
+      verify(apiProvider.get(
+        path: "/search-users",
+        parameters: {
+          "name": null,
+          "skills": "a,b,c",
+          "rows": "100",
+          "next_page": null
+        },
+      ));
+    });
+    test('should return empty session for no search result', () async {
+      // arrange
+      final jsonEmptySession = "users/users_search_empty.json";
+      final jsonData = await JsonUtil.getJson(from: jsonEmptySession);
+      final actual = right(UserSearchSessionModel.fromJson(jsonData));
+      final options = UserSearchOptions(skills: ["a", "b", "c"]);
+      when(apiProvider.get(
+        path: anyNamed('path'),
+        headers: anyNamed('headers'),
+        parameters: anyNamed('parameters'),
+      )).thenAnswer((_) => JsonUtil.getString(from: jsonEmptySession));
+      // act
+      final matcher = await sut.search(options);
+      // assert
+      expect(actual, matcher);
+    });
+    test('should return full session for search with results', () async {
+      // arrange
+      final jsonData = await JsonUtil.getJson(from: jsonFile);
+      final actual = right(UserSearchSessionModel.fromJson(jsonData));
+      final options = UserSearchOptions(skills: ["a", "b", "c"]);
+      when(apiProvider.get(
+        path: anyNamed('path'),
+        headers: anyNamed('headers'),
+        parameters: anyNamed('parameters'),
+      )).thenAnswer((_) => JsonUtil.getString(from: jsonFile));
+      // act
+      final matcher = await sut.search(options);
+      // assert
+      expect(actual, matcher);
+    });
+  });
+}
