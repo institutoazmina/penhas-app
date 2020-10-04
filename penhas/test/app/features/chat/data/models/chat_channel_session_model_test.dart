f56973ed3c3d7e805b8f96e1c3ac6542bea1cd3a
Solli Honorio <shonorio@gmail.com>
2020-10-04 21:06:53 +0100

    ChatChannelSessionModel should return a valid model with a valid JSON

--- penhas/test/app/features/chat/data/models/chat_channel_session_model_test.dart
+++ penhas/test/app/features/chat/data/models/chat_channel_session_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:penhas/app/features/chat/data/models/chat_channel_session_model.dart';
+import 'package:penhas/app/features/chat/data/models/chat_message_model.dart';
 import 'package:penhas/app/features/chat/data/models/chat_user_model.dart';
 import 'package:penhas/app/features/chat/domain/entities/chat_channel_session_entity.dart';
 
@@ -23,7 +24,7 @@ void main() {
       expect(model, isA<ChatChannelSessionEntity>());
     });
 
-    test('should return a valid model with a valid JSON with empty channels',
+    test('should return a valid model with a valid JSON with empty messages',
         () async {
       // arrange
       final jsonFile = "chat/chat_read_channel_empty.json";
@@ -40,6 +41,41 @@ void main() {
             headerWarning: "",
             isBlockable: true,
             lastMessageEtag: "4df2ae",
+          ),
+          user: ChatUserModel(
+            activity: "há pouco tempo",
+            nickname: "Julia",
+            avatar: "https://api.example.com/avatar/padrao.svg",
+            userId: 3191,
+          ));
+      // act
+      final matcher = ChatChannelSessionModel.fromJson(jsonData);
+      // assert
+      expect(actual, matcher);
+    });
+    test('should return a valid model with a valid JSON', () async {
+      // arrange
+      final jsonFile = "chat/chat_read_channel.json";
+      final jsonData = await JsonUtil.getJson(from: jsonFile);
+      final actual = ChatChannelSessionModel(
+          hasMore: false,
+          newer: "--my-pagination-token--",
+          older: "--my-older-pagination-token--",
+          messages: [
+            ChatMessageModel(
+              id: 172,
+              isMe: true,
+              message: "Ola mundo!",
+              time: DateTime.parse("2020-10-04T09:30:54Z"),
+            )
+          ],
+          metadata: ChatChannelSessionMetadataModel(
+            canSendMessage: true,
+            didBlocked: false,
+            headerMessage: "Minha mensagem",
+            headerWarning: "Meu alerta",
+            isBlockable: true,
+            lastMessageEtag: "4a295f",
           ),
           user: ChatUserModel(
             activity: "há pouco tempo",
