e2d1af75e92ae2294e0d287aff7381ee6de2f916
Solli Honorio <shonorio@gmail.com>
2020-10-04 19:59:16 +0100

    ChatChannelSessionModel should a subclass of ChatChannelSessionEntity

--- penhas/test/app/features/chat/data/models/chat_channel_session_model_test.dart
+++ penhas/test/app/features/chat/data/models/chat_channel_session_model_test.dart
@@ -1,7 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:penhas/app/features/chat/data/models/chat_channel_session_model.dart';
+import 'package:penhas/app/features/chat/domain/entities/chat_channel_session_entity.dart';
 
 void main() {
   setUp(() {});
 
-  group('', () {});
+  group('ChatChannelSessionModel', () {
+    test('should a subclass of ChatChannelSessionEntity', () async {
+      // act
+      final model = ChatChannelSessionModel(
+        hasMore: null,
+        newer: null,
+        older: null,
+        messages: null,
+        metadata: null,
+        user: null,
+      );
+      // assert
+      expect(model, isA<ChatChannelSessionEntity>());
+    });
+  });
 }
