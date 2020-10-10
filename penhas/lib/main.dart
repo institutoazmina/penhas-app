bc90a45d9ca82a78dd9d22a64e2c8aefdf898631
Solli Honorio <shonorio@gmail.com>
2020-10-10 18:11:51 +0100

    desabilitado o firebase_crashlytics porque a nova versão está exigindo configurações que não posso fazer neste momento

--- penhas/lib/main.dart
+++ penhas/lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:penhas/app/app_module.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() {
-  // Set `enableInDevMode` to true to see reports while in debug mode
-  // This is only to be used for confirming that reports are being
-  // submitted as expected. It is not intended to be used for everyday
-  // development.
-  Crashlytics.instance.enableInDevMode = true;
-
-  // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
-
   runApp(
     ModularApp(module: AppModule()),
   );
