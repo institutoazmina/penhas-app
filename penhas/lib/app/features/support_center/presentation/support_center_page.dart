976e18d0501937e8c067818e914737f7660a1c96
Solli Honorio <shonorio@gmail.com>
2020-10-30 01:23:37 +0000

    setup inicial do Google Map, configuração para Android pronta

--- penhas/lib/app/features/support_center/presentation/support_center_page.dart
+++ penhas/lib/app/features/support_center/presentation/support_center_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobx/mobx.dart';
 import 'package:penhas/app/features/authentication/presentation/shared/page_progress_indicator.dart';
 import 'package:penhas/app/features/authentication/presentation/shared/snack_bar_handler.dart';
@@ -20,27 +21,57 @@ class _SupportCenterPageState
     with SnackBarHandler {
   List<ReactionDisposer> _disposers;
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: SafeArea(
-        child: Observer(
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: height,
+      width: width,
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: Observer(
           builder: (_) {
             return PageProgressIndicator(
               progressState: controller.progressState,
-              child: Column(
+              child: Stack(
                 children: [
+                  GoogleMap(
+                    initialCameraPosition:
+                        CameraPosition(target: LatLng(0.0, 0.0)),
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: false,
+                    mapType: MapType.normal,
+                    zoomGesturesEnabled: true,
+                    zoomControlsEnabled: false,
+                    onMapCreated: (GoogleMapController controller) {
+                      mapController = controller;
+                    },
+                  ),
                   SupportCenterInputFilter(
                     totalOfFilter: controller.categoriesSelected,
                     onFilterAction: controller.onFilterAction,
                     onKeywordsAction: controller.onKeywordsAction,
                   ),
-                  Container(
-                    height: 80,
-                    color: Colors.yellowAccent,
-                  ),
+                  Positioned(
+                    bottom: 40,
+                    right: 12,
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Container(color: Colors.amber, width: 44, height: 44),
+                          Container(color: Colors.blue, width: 44, height: 44),
+                          Container(
+                              color: Colors.cyanAccent, width: 44, height: 44),
+                          Container(
+                              color: Colors.deepPurple, width: 44, height: 44),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             );
