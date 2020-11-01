a4cffe68ddebff7b2356f5cfb14c7d61b3a6ca5c
Solli Honorio <shonorio@gmail.com>
2020-11-01 18:46:33 +0000

    estados do ponto de apoio

---
+++ penhas/lib/app/features/support_center/domain/states/support_center_state.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'support_center_state.freezed.dart';
+
+@freezed
+abstract class SupportCenterState with _$SupportCenterState {
+  const factory SupportCenterState.initial() = _Initial;
+  const factory SupportCenterState.loaded() = _Loaded;
+  const factory SupportCenterState.error(String message) = _ErrorDetails;
+  const factory SupportCenterState.gpsError(String message) = _GpsError;
+}
