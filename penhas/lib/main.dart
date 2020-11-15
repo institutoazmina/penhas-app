cc0d9b83b410aa6497225075d64716a6b75d0c41
Solli Honorio <shonorio@gmail.com>
2020-11-15 14:32:10 +0000

    Compilando para executar no iOS

--- penhas/lib/main.dart
+++ penhas/lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:penhas/app/app_module.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   // Pass all uncaught errors from the framework to Crashlytics.
+  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   runApp(
