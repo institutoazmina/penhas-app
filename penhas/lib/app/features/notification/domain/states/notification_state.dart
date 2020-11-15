9432cc32156e1833ddd33a6aaec0ac1c49b76613
Solli Honorio <shonorio@gmail.com>
2020-11-15 17:28:02 +0000

    setup do state

--- penhas/lib/app/features/notification/domain/states/notification_state.dart
+++ penhas/lib/app/features/notification/domain/states/notification_state.dart
@@ -1,11 +1,11 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'notification_state.freezed.dart';
+part 'notification_state.freezed.dart';
 
-// @freezed
-// abstract class NotificationState with _$NotificationState {
-//   const factory NotificationState.initial() = _Initial;
-//   const factory NotificationState.loaded() = _Loaded;
-//   const factory NotificationState.error(String message) = _ErrorDetails;
-// }
+@freezed
+abstract class NotificationState with _$NotificationState {
+  const factory NotificationState.initial() = _Initial;
+  const factory NotificationState.loaded() = _Loaded;
+  const factory NotificationState.error(String message) = _ErrorDetails;
+}
