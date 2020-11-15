d6a8730b8dcf5da4d4c331824fc8612cf7095241
Solli Honorio <shonorio@gmail.com>
2020-11-15 12:12:26 +0000

    setup crashlytics

--- penhas/lib/main.dart
+++ penhas/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:penhas/app/app_module.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() {
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(
     ModularApp(module: AppModule()),
   );
