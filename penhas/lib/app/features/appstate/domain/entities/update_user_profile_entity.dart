6183374319d0ca597e9cd77239c663c8e00e5a00
Solli Honorio <shonorio@gmail.com>
2020-11-27 00:24:48 +0000

    Classe de dados para realizar a atualização do profile

---
+++ penhas/lib/app/features/appstate/domain/entities/update_user_profile_entity.dart
@@ -0,0 +1,24 @@
+class UpdateUserProfileEntity {
+  final String nickName;
+  final String email;
+  final String newPassword;
+  final String oldPassword;
+  final String minibio;
+  final String race;
+  final List<String> skills;
+
+  UpdateUserProfileEntity({
+    this.nickName,
+    this.email,
+    this.newPassword,
+    this.oldPassword,
+    this.minibio,
+    this.race,
+    this.skills,
+  }) {
+    if ((email != null || newPassword != null) &&
+        (oldPassword == null || oldPassword.isEmpty)) {
+      throw ('Precisa da senha atual para alterar a senha ou email');
+    }
+  }
+}
