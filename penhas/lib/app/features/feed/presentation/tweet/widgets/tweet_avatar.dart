fb0c2cbb5df0f6fd8e1a611aa3ebb040e7173c40
Solli Honorio <shonorio@gmail.com>
2020-11-29 14:45:38 +0000

    apresenta a tela de perfil para o avatar não anônimo

--- penhas/lib/app/features/feed/presentation/tweet/widgets/tweet_avatar.dart
+++ penhas/lib/app/features/feed/presentation/tweet/widgets/tweet_avatar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:penhas/app/features/feed/domain/entities/tweet_entity.dart';
+import 'package:penhas/app/features/feed/domain/states/feed_router_type.dart';
 import 'package:penhas/app/shared/design_system/colors.dart';
 
 class TweetAvatar extends StatelessWidget {
@@ -16,7 +18,7 @@ class TweetAvatar extends StatelessWidget {
     return CircleAvatar(
       backgroundColor: Color.fromRGBO(239, 239, 239, 1.0),
       radius: 24.0,
-      child: avatar(),
+      child: tweet.anonymous ? anonymousAvatar() : authenticatedAvatar(),
     );
   }
 }
@@ -29,18 +31,27 @@ extension _PrivateMethod on TweetAvatar {
       height: 36,
     );
   }
-}
-/*
 
-void _showUserProfile() {
+  Widget anonymousAvatar() {
+    return avatar();
+  }
+
+  Widget authenticatedAvatar() {
+    return FlatButton(
+      child: avatar(),
+      onPressed: () => showUserProfile(),
+      color: Colors.transparent,
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      padding: EdgeInsets.zero,
+    );
+  }
+
+  void showUserProfile() {
     final routeOption = FeedRouterType.profile(tweet.clientId);
-
     Modular.to.pushNamed(
       "/mainboard/tweet/perfil_chat",
       arguments: routeOption,
     );
   }
-
-
-                      avatar: 
-*/
+}
