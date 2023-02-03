12f5e2754d94599e8e4386106e675eedc60ba370
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-03 18:32:03 +0100

    chore: removido código não utilizado

--- lib/app/core/extension/either.dart
+++ lib/app/core/extension/either.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 
 extension EitherExtension<L extends Object, R> on Either<L, R> {
-  R? getOrNull() => fold((l) => null, (r) => r);
   R get() => fold((l) => throw l, (r) => r);
 }
