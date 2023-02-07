4e503901f088e708de1b823cab9f1f9c40efd229
Solli Moreira Honorio <solli.moreirahonorio@zurich.com>
2023-02-07 21:36:20 +0100

    test: safeParseInt retorna o valor "padrão" quando definido

--- test/app/core/extension/safely_parser_test.dart
+++ test/app/core/extension/safely_parser_test.dart
@@ -3,21 +3,22 @@ import 'package:penhas/app/core/extension/safely_parser.dart';
 
 void main() {
   group('SafelyParser extension', () {
-    test('safeParseDouble should parse double from string or number', () {
+    test('safeParseDouble parse double from string or number', () {
       expect((1.0).safeParseDouble(), 1.0);
       expect(('1.0').safeParseDouble(), 1.0);
       expect(('not a number').safeParseDouble(), isNull);
       expect((null).safeParseDouble(), isNull);
     });
 
-    test('safeParseInt should parse int from string or number', () {
+    test('safeParseInt parse int from string or number', () {
       expect((1).safeParseInt(), 1);
       expect(('1').safeParseInt(), 1);
+      expect(('default value').safeParseInt(def: 42), 42);
       expect(('not an int').safeParseInt(), 0);
       expect((null).safeParseInt(), 0);
     });
 
-    test('safeParseBool should return true for 1, false for anything else', () {
+    test('safeParseBool return true for 1, false for anything else', () {
       expect((1).safeParseBool(), isTrue);
       expect((0).safeParseBool(), isFalse);
       expect(('1').safeParseBool(), isFalse);
