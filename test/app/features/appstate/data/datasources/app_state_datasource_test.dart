3f0861e61015a71cdfd51d95a50bace8793fea9f
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-11 18:33:10 +0100

    test: AppStateDataSource get ApiProviderException for http code error not mapped

--- test/app/features/appstate/data/datasources/app_state_datasource_test.dart
+++ test/app/features/appstate/data/datasources/app_state_datasource_test.dart
@@ -4,50 +4,46 @@ import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/core/error/exceptions.dart';
 import 'package:penhas/app/core/network/api_server_configure.dart';
 import 'package:penhas/app/features/appstate/data/datasources/app_state_data_source.dart';
-import 'package:penhas/app/features/appstate/data/model/app_state_model.dart';
 
 import '../../../../../utils/json_util.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
 
-class MockIApiServerConfigure extends Mock implements IApiServerConfigure {}
+class MockApiServerConfigure extends Mock implements IApiServerConfigure {}
 
 void main() {
   late String bodyContent;
+  late Uri serverEndpoint;
   late http.Client apiClient;
-  late IAppStateDataSource dataSource;
-  late IApiServerConfigure serverConfigure;
-  late When<Future<http.Response>> mockGetAction;
-  final Uri serverEndpoint = Uri.https('api.anyserver.io', '/');
+  late IApiServerConfigure serverConfiguration;
+  late IAppStateDataSource sut;
 
   setUpAll(() {
     registerFallbackValue(Uri());
   });
 
   setUp(() {
+    apiClient = MockHttpClient();
+    serverConfiguration = MockApiServerConfigure();
+    serverEndpoint = Uri.https('api.anyserver.io', '/');
+
+    sut = AppStateDataSource(
+      apiClient: apiClient,
+      serverConfiguration: serverConfiguration,
+    );
+
     bodyContent =
         JsonUtil.getStringSync(from: 'profile/about_with_quiz_session.json');
 
-    apiClient = MockHttpClient();
-    serverConfigure = MockIApiServerConfigure();
-    dataSource = AppStateDataSource(
-      apiClient: apiClient,
-      serverConfiguration: serverConfigure,
-    );
-
-    // MockApiServerConfigure configuration
-    when(() => serverConfigure.baseUri).thenReturn(serverEndpoint);
-    when(() => serverConfigure.apiToken)
-        .thenAnswer((_) async => 'my.very.strong');
-    when(() => serverConfigure.userAgent)
+    when(() => serverConfiguration.userAgent)
         .thenAnswer((_) async => 'iOS 11.4/Simulator/1.0.0');
-
-    mockGetAction =
-        when(() => apiClient.get(any(), headers: any(named: 'headers')));
+    when(() => serverConfiguration.apiToken)
+        .thenAnswer((_) async => 'my.very.strong');
+    when(() => serverConfiguration.baseUri).thenReturn(serverEndpoint);
   });
 
   Future<Map<String, String>> _setUpHttpHeader() async {
-    final userAgent = await serverConfigure.userAgent;
+    final userAgent = await serverConfiguration.userAgent;
     return {
       'User-Agent': userAgent,
       'Content-Type': 'application/json; charset=utf-8',
@@ -64,7 +60,7 @@ void main() {
   }
 
   void _setUpMockHttpClientSuccess200() {
-    mockGetAction.thenAnswer(
+    when(() => apiClient.get(any(), headers: any(named: 'headers'))).thenAnswer(
       (_) async => http.Response(
         bodyContent,
         200,
@@ -74,7 +70,7 @@ void main() {
   }
 
   void _setUpMockHttpClientFailedWithHttp({required int code}) {
-    mockGetAction.thenAnswer(
+    when(() => apiClient.get(any(), headers: any(named: 'headers'))).thenAnswer(
       (_) async => http.Response(
         '{"status": $code, "error":"Some error message"}',
         code,
@@ -92,41 +88,33 @@ void main() {
         final loginUri = _setupHttpRequest();
         _setUpMockHttpClientSuccess200();
         // act
-        await dataSource.check();
+        await sut.check();
         // assert
         verify(() => apiClient.get(loginUri, headers: headers)).called(1);
       },
     );
-    test(
-      'get AppStateModel for valid session',
-      () async {
-        // arrange
-        _setUpMockHttpClientSuccess200();
-        final jsonData = await JsonUtil.getJson(
-            from: 'profile/about_with_quiz_session.json');
-        final expected = AppStateModel.fromJson(jsonData);
+
+    test('get ApiProviderSessionError for invalid session', () {
+      // arrange
+      final sessionHttpCodeError = [401, 403];
+      for (final httpCode in sessionHttpCodeError) {
+        _setUpMockHttpClientFailedWithHttp(code: httpCode);
         // act
-        final result = await dataSource.check();
-        // assert
-        expect(result, expected);
-      },
-    );
-    test(
-      'get ApiProviderSessionError for invalid session',
-      () async {
-        // arrange
-        final sessionHttpCodeError = [401];
-        for (final httpCode in sessionHttpCodeError) {
-          _setUpMockHttpClientFailedWithHttp(code: httpCode);
-          // act
-          // final sut = dataSource.check;
-          // assert
-          expect(
-            () async => await dataSource.check(),
-            throwsA(isA<ApiProviderSessionError>()),
-          );
-        }
-      },
-    );
+        expect(
+          () async => await sut.check(),
+          throwsA(isA<ApiProviderSessionError>()),
+        );
+      }
+    });
+
+    test('get ApiProviderException for http code error not mapped', () {
+      // arrange
+      _setUpMockHttpClientFailedWithHttp(code: 503);
+      // act
+      expect(
+        () async => await sut.check(),
+        throwsA(isA<ApiProviderException>()),
+      );
+    });
   });
 }
