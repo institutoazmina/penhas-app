74ec52961bc16831edab48e8661f4875b388927c
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-11 22:53:03 +0100

    test: cobertura de teste para o AppStateDataSource update()

--- test/app/features/appstate/data/datasources/app_state_datasource_test.dart
+++ test/app/features/appstate/data/datasources/app_state_datasource_test.dart
@@ -4,6 +4,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/core/error/exceptions.dart';
 import 'package:penhas/app/core/network/api_server_configure.dart';
 import 'package:penhas/app/features/appstate/data/datasources/app_state_data_source.dart';
+import 'package:penhas/app/features/appstate/domain/entities/update_user_profile_entity.dart';
 
 import '../../../../../utils/json_util.dart';
 
@@ -42,11 +43,20 @@ void main() {
     when(() => serverConfiguration.baseUri).thenReturn(serverEndpoint);
   });
 
-  Future<Map<String, String>> _setUpHttpHeader() async {
+  Future<Map<String, String>> _setUpCheckHttpHeader() async {
     final userAgent = await serverConfiguration.userAgent;
     return {
       'User-Agent': userAgent,
       'Content-Type': 'application/json; charset=utf-8',
+      'X-Api-Key': 'my.very.strong',
+    };
+  }
+
+  Future<Map<String, String>> _setUpUpdateHttpHeader() async {
+    final userAgent = await serverConfiguration.userAgent;
+    return {
+      'User-Agent': userAgent,
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
       'X-Api-Key': 'my.very.strong',
     };
   }
@@ -59,7 +69,7 @@ void main() {
     );
   }
 
-  void _setUpMockHttpClientSuccess200() {
+  void _setUpCheckSuccessResponse() {
     when(() => apiClient.get(any(), headers: any(named: 'headers'))).thenAnswer(
       (_) async => http.Response(
         bodyContent,
@@ -69,7 +79,21 @@ void main() {
     );
   }
 
-  void _setUpMockHttpClientFailedWithHttp({required int code}) {
+  void _setUpUpdateSuccessResponse() {
+    when(() => apiClient.put(
+          any(),
+          headers: any(named: 'headers'),
+          body: any(named: 'body'),
+        )).thenAnswer(
+      (_) async => http.Response(
+        bodyContent,
+        200,
+        headers: {'content-type': 'application/json; charset=utf-8'},
+      ),
+    );
+  }
+
+  void _setUpCheckFailureResponse({required int code}) {
     when(() => apiClient.get(any(), headers: any(named: 'headers'))).thenAnswer(
       (_) async => http.Response(
         '{"status": $code, "error":"Some error message"}',
@@ -79,42 +103,116 @@ void main() {
     );
   }
 
-  group(AppStateDataSource, () {
-    test(
-      'perform a GET with X-API-Key and application/json header ',
-      () async {
-        // arrange
-        final headers = await _setUpHttpHeader();
-        final loginUri = _setupHttpRequest();
-        _setUpMockHttpClientSuccess200();
-        // act
-        await sut.check();
-        // assert
-        verify(() => apiClient.get(loginUri, headers: headers)).called(1);
-      },
+  void _setUpUpdateFailureResponse({required int code}) {
+    when(() => apiClient.put(
+          any(),
+          headers: any(named: 'headers'),
+          body: any(named: 'body'),
+        )).thenAnswer(
+      (_) async => http.Response(
+        '{"status": $code, "error":"Some error message"}',
+        code,
+        headers: {'content-type': 'application/json; charset=utf-8'},
+      ),
     );
+  }
 
-    test('get ApiProviderSessionError for invalid session', () {
-      // arrange
-      final sessionHttpCodeError = [401, 403];
-      for (final httpCode in sessionHttpCodeError) {
-        _setUpMockHttpClientFailedWithHttp(code: httpCode);
+  group(AppStateDataSource, () {
+    group('check()', () {
+      test(
+        'perform a GET with X-API-Key and application/json header ',
+        () async {
+          // arrange
+          final headers = await _setUpCheckHttpHeader();
+          final stateUri = _setupHttpRequest();
+          _setUpCheckSuccessResponse();
+          // act
+          await sut.check();
+          // assert
+          verify(() => apiClient.get(stateUri, headers: headers)).called(1);
+        },
+      );
+
+      test('get ApiProviderSessionError for invalid session', () {
+        // arrange
+        final sessionHttpCodeError = [401, 403];
+        for (final httpCode in sessionHttpCodeError) {
+          _setUpCheckFailureResponse(code: httpCode);
+          // act
+          expect(
+            () async => await sut.check(),
+            throwsA(isA<ApiProviderSessionError>()),
+          );
+        }
+      });
+
+      test('get ApiProviderException for http code error not mapped', () {
+        // arrange
+        _setUpCheckFailureResponse(code: 503);
         // act
         expect(
           () async => await sut.check(),
-          throwsA(isA<ApiProviderSessionError>()),
+          throwsA(isA<ApiProviderException>()),
         );
-      }
+      });
     });
 
-    test('get ApiProviderException for http code error not mapped', () {
-      // arrange
-      _setUpMockHttpClientFailedWithHttp(code: 503);
-      // act
-      expect(
-        () async => await sut.check(),
-        throwsA(isA<ApiProviderException>()),
+    group('update()', () {
+      test(
+        'perform a PUT with X-API-Key and application/x-www-form-urlencoded; charset=utf-8',
+        () async {
+          // arrange
+          final headers = await _setUpUpdateHttpHeader();
+          final stateUri = _setupHttpRequest();
+          final profile = UpdateUserProfileEntity(
+            nickName: 'foo',
+            email: 'foo@foo.com',
+            newPassword: 'strong_password',
+            oldPassword: 'old-password',
+            race: 'foo',
+            skills: ['asd', 'zxc'],
+            minibio: 'minha mini bio',
+          );
+          _setUpUpdateSuccessResponse();
+          // act
+          await sut.update(profile);
+          // assert
+          verify(
+            () => apiClient.put(
+              stateUri,
+              headers: headers,
+              body:
+                  'apelido=foo&minibio=minha%20mini%20bio&raca=foo&skills=asd%2Czxc&senha_atual=old-password&senha=strong_password&email=foo%40foo.com',
+              encoding: null,
+            ),
+          ).called(1);
+        },
       );
+
+      test('get ApiProviderSessionError for invalid session', () async {
+        // arrange
+        final sessionHttpCodeError = [401, 403];
+        final profile = UpdateUserProfileEntity();
+        for (final httpCode in sessionHttpCodeError) {
+          _setUpUpdateFailureResponse(code: httpCode);
+          // act
+          expect(
+            () async => await sut.update(profile),
+            throwsA(isA<ApiProviderSessionError>()),
+          );
+        }
+      });
+
+      test('get ApiProviderException for invalid session', () async {
+        // arrange
+        final profile = UpdateUserProfileEntity();
+        _setUpUpdateFailureResponse(code: 503);
+        // act
+        expect(
+          () async => await sut.update(profile),
+          throwsA(isA<ApiProviderException>()),
+        );
+      });
     });
   });
 }
