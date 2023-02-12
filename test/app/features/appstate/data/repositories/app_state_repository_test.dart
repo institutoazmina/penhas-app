0902171a7aa6de27adc0ecc5e9e1cc9b80da7254
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-12 10:22:41 +0100

    test: cobertura de teste do AppStateRepository

--- test/app/features/appstate/data/repositories/app_state_repository_test.dart
+++ test/app/features/appstate/data/repositories/app_state_repository_test.dart
@@ -8,6 +8,7 @@ import 'package:penhas/app/features/appstate/data/datasources/app_state_data_sou
 import 'package:penhas/app/features/appstate/data/model/app_state_model.dart';
 import 'package:penhas/app/features/appstate/data/repositories/app_state_repository.dart';
 import 'package:penhas/app/features/appstate/domain/entities/app_state_entity.dart';
+import 'package:penhas/app/features/appstate/domain/entities/update_user_profile_entity.dart';
 import 'package:penhas/app/features/appstate/domain/repositories/i_app_state_repository.dart';
 
 import '../../../../../utils/json_util.dart';
@@ -21,6 +22,10 @@ void main() {
   late IAppStateRepository sut;
   late IAppStateDataSource dataSource;
   late Map<String, dynamic> jsonData;
+
+  setUpAll(() {
+    registerFallbackValue(UpdateUserProfileEntity());
+  });
 
   setUp(() async {
     networkInfo = MockNetworkInfo();
@@ -70,6 +75,98 @@ void main() {
         final expected = left(ServerSideSessionFailed());
         // act
         final received = await sut.check();
+        // assert
+        expect(received, expected);
+      });
+
+      test(
+          'return ServerSideFormFieldValidationFailure for ApiProviderException',
+          () async {
+        // arrange
+        const exception = ApiProviderException(
+          bodyContent: {
+            'error': 'other_error',
+            'message': 'my message',
+          },
+        );
+        when(() => dataSource.check()).thenThrow(exception);
+        final expected = left(
+          ServerSideFormFieldValidationFailure(
+            error: 'other_error',
+            message: 'my message',
+          ),
+        );
+        // act
+        final received = await sut.check();
+        // assert
+        expect(received, expected);
+      });
+    });
+
+    group('update', () {
+      test('return valid AppStateEntity for a valid session', () async {
+        // arrange
+        final expectedModel = AppStateModel.fromJson(jsonData);
+        final AppStateEntity expectedEntity = expectedModel;
+        when(() => dataSource.update(any()))
+            .thenAnswer((_) async => expectedModel);
+        final profile = UpdateUserProfileEntity();
+        // act
+        final received = await sut.update(profile);
+        // assert
+        expect(received, right(expectedEntity));
+      });
+
+      test('return ServerSideSessionFailed for a invalid session', () async {
+        // arrange
+        when(() => dataSource.update(any()))
+            .thenThrow(ApiProviderSessionError());
+        final expected = left(ServerSideSessionFailed());
+        final profile = UpdateUserProfileEntity();
+        // act
+        final received = await sut.update(profile);
+        // assert
+        expect(received, expected);
+      });
+
+      test('return ServerSideSessionFailed for a invalid JWT', () async {
+        // arrange
+        when(() => dataSource.update(any())).thenThrow(
+          const ApiProviderException(
+            bodyContent: {
+              'error': 'expired_jwt',
+              'nessage': 'Bad request - Invalid JWT'
+            },
+          ),
+        );
+        final expected = left(ServerSideSessionFailed());
+        final profile = UpdateUserProfileEntity();
+        // act
+        final received = await sut.update(profile);
+        // assert
+        expect(received, expected);
+      });
+
+      test(
+          'return ServerSideFormFieldValidationFailure for ApiProviderException',
+          () async {
+        // arrange
+        const exception = ApiProviderException(
+          bodyContent: {
+            'error': 'other_error',
+            'message': 'my message',
+          },
+        );
+        when(() => dataSource.update(any())).thenThrow(exception);
+        final expected = left(
+          ServerSideFormFieldValidationFailure(
+            error: 'other_error',
+            message: 'my message',
+          ),
+        );
+        final profile = UpdateUserProfileEntity();
+        // act
+        final received = await sut.update(profile);
         // assert
         expect(received, expected);
       });
