aae76d9d3c5ec46d47cd208167165967c8ad85e5
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-16 14:15:03 +0100

    test: TweetDataSource migrado para mocktail

--- test/app/features/feed/data/datasources/tweet_data_source_reply_test.dart
+++ test/app/features/feed/data/datasources/tweet_data_source_reply_test.dart
@@ -1,34 +1,44 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/network/api_server_configure.dart';
 import 'package:penhas/app/features/feed/data/datasources/tweet_data_source.dart';
 import 'package:penhas/app/features/feed/data/models/tweet_model.dart';
 import 'package:penhas/app/features/feed/domain/entities/tweet_engage_request_option.dart';
 import 'package:penhas/app/features/feed/domain/entities/tweet_entity.dart';
 
-import '../../../../../utils/helper.mocks.dart';
 import '../../../../../utils/json_util.dart';
 
+class MockHttpClient extends Mock implements http.Client {}
+
+class MockApiServerConfigure extends Mock implements IApiServerConfigure {}
+
 void main() {
-  late final MockHttpClient apiClient = MockHttpClient();
+  late Uri serverEndpoint;
+  late http.Client apiClient;
   late ITweetDataSource dataSource;
-  late final MockIApiServerConfigure serverConfigure =
-      MockIApiServerConfigure();
-  late final Uri serverEndpoint = Uri.https('api.anyserver.io', '/');
+  late IApiServerConfigure serverConfigure;
   const String sessionToken = 'my_really.long.JWT';
 
   setUp(() {
+    apiClient = MockHttpClient();
+    serverConfigure = MockApiServerConfigure();
+    serverEndpoint = Uri.https('api.anyserver.io', '/');
     dataSource = TweetDataSource(
       apiClient: apiClient,
       serverConfiguration: serverConfigure,
     );
 
     // MockApiServerConfigure configuration
-    when(serverConfigure.baseUri).thenAnswer((_) => serverEndpoint);
-    when(serverConfigure.apiToken)
+    when(() => serverConfigure.baseUri).thenAnswer((_) => serverEndpoint);
+    when(() => serverConfigure.apiToken)
         .thenAnswer((_) => Future.value(sessionToken));
-    when(serverConfigure.userAgent)
+    when(() => serverConfigure.userAgent)
         .thenAnswer((_) => Future.value('iOS 11.4/Simulator/1.0.0'));
+  });
+
+  setUpAll(() {
+    registerFallbackValue(Uri());
   });
 
   Future<Map<String, String>> _setUpHttpHeader() async {
@@ -40,7 +50,7 @@ void main() {
     };
   }
 
-  Uri _setuHttpRequest(String path, Map<String, String> queryParameters) {
+  Uri _setUpHttpRequest(String path, Map<String, String> queryParameters) {
     return Uri(
       scheme: serverEndpoint.scheme,
       host: serverEndpoint.host,
@@ -49,18 +59,14 @@ void main() {
     );
   }
 
-  PostExpectation<Future<http.Response>> _mockPostRequest() {
-    return when(
-      apiClient.post(
-        any,
-        headers: anyNamed('headers'),
-        body: anyNamed('body'),
-      ),
-    );
-  }
-
   void _setUpMockPostHttpClientSuccess200(String? bodyContent) {
-    _mockPostRequest().thenAnswer(
+    when(
+      () => apiClient.post(
+        any(),
+        headers: any(named: 'headers'),
+        body: any(named: 'body'),
+      ),
+    ).thenAnswer(
       (_) async => http.Response(
         bodyContent!,
         200,
@@ -71,7 +77,7 @@ void main() {
     );
   }
 
-  group('FeedDataSource', () {
+  group(TweetDataSource, () {
     group('reply()', () {
       String? bodyContent;
       TweetEngageRequestOption? requestOption;
@@ -84,45 +90,51 @@ void main() {
           message: 'um breve comentario',
         );
       });
-      test('should perform a POST with X-API-Key', () async {
-        // arrange
-        final endPointPath = '/timeline/${requestOption!.tweetId}/comment';
-        final bodyRequest = Uri.encodeComponent('um breve comentario');
-        final headers = await _setUpHttpHeader();
-        final request = _setuHttpRequest(endPointPath, {});
-        _setUpMockPostHttpClientSuccess200(bodyContent);
-        // act
-        await dataSource.reply(option: requestOption);
-        // assert
-        verify(
-          apiClient.post(
-            request,
-            headers: headers,
-            body: 'content=$bodyRequest',
-          ),
-        );
-      });
-      test('should get a valid ValidField for a successful request', () async {
-        // arrange
-        _setUpMockPostHttpClientSuccess200(bodyContent);
-        final expected = TweetModel(
-          id: '200608T1809090001',
-          userName: 'rosa',
-          clientId: 551,
-          createdAt: '2020-06-08 18:09:09',
-          totalReply: 0,
-          totalLikes: 0,
-          anonymous: false,
-          content: 'um breve comentario',
-          avatar: 'https://elasv2-api.appcivico.com/avatar/padrao.svg',
-          meta: const TweetMeta(liked: false, owner: true),
-          lastReply: const [],
-        );
-        // act
-        final received = await dataSource.reply(option: requestOption);
-        // assert
-        expect(expected, received);
-      });
+      test(
+        'perform a POST with X-API-Key',
+        () async {
+          // arrange
+          final endPointPath = '/timeline/${requestOption!.tweetId}/comment';
+          final bodyRequest = Uri.encodeComponent('um breve comentario');
+          final headers = await _setUpHttpHeader();
+          final request = _setUpHttpRequest(endPointPath, {});
+          _setUpMockPostHttpClientSuccess200(bodyContent);
+          // act
+          await dataSource.reply(option: requestOption);
+          // assert
+          verify(
+            () => apiClient.post(
+              request,
+              headers: headers,
+              body: 'content=$bodyRequest',
+            ),
+          ).called(1);
+        },
+      );
+      test(
+        'get a valid ValidField for a successful request',
+        () async {
+          // arrange
+          _setUpMockPostHttpClientSuccess200(bodyContent);
+          final expected = TweetModel(
+            id: '200608T1809090001',
+            userName: 'rosa',
+            clientId: 551,
+            createdAt: '2020-06-08 18:09:09',
+            totalReply: 0,
+            totalLikes: 0,
+            anonymous: false,
+            content: 'um breve comentario',
+            avatar: 'https://elasv2-api.appcivico.com/avatar/padrao.svg',
+            meta: const TweetMeta(liked: false, owner: true),
+            lastReply: const [],
+          );
+          // act
+          final received = await dataSource.reply(option: requestOption);
+          // assert
+          expect(expected, received);
+        },
+      );
     });
   });
 }
