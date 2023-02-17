268f8dd44835dd9ace58dc0d567e696fc2ece21b
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-17 13:09:14 +0100

    test: GuardianDataSource migrado para mocktail

--- test/app/features/help_center/data/datasources/guardian_datasources_create_test.dart
+++ test/app/features/help_center/data/datasources/guardian_datasources_create_test.dart
@@ -1,33 +1,42 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/network/api_server_configure.dart';
 import 'package:penhas/app/features/help_center/data/datasources/guardian_data_source.dart';
 import 'package:penhas/app/features/help_center/data/models/alert_model.dart';
 import 'package:penhas/app/features/help_center/domain/entities/guardian_session_entity.dart';
 
-import '../../../../../utils/helper.mocks.dart';
 import '../../../../../utils/json_util.dart';
 
+class MockHttpClient extends Mock implements http.Client {}
+
+class MockApiServerConfigure extends Mock implements IApiServerConfigure {}
+
 void main() {
-  late final MockHttpClient apiClient = MockHttpClient();
-  late final MockIApiServerConfigure serverConfigure =
-      MockIApiServerConfigure();
+  late Uri serverEndpoint;
+  late http.Client apiClient;
+  late IApiServerConfigure serverConfigure;
   late IGuardianDataSource dataSource;
-  final Uri serverEndpoint = Uri.https('api.anyserver.io', '/');
-  const String sessionToken = 'my_really.long.JWT';
+  const sessionToken = 'my_really.long.JWT';
 
   setUp(() {
+    apiClient = MockHttpClient();
+    serverConfigure = MockApiServerConfigure();
+    serverEndpoint = Uri.https('api.anyserver.io', '/');
     dataSource = GuardianDataSource(
       apiClient: apiClient,
       serverConfiguration: serverConfigure,
     );
 
     // MockApiServerConfigure configuration
-    when(serverConfigure.baseUri).thenAnswer((_) => serverEndpoint);
-    when(serverConfigure.apiToken)
-        .thenAnswer((_) => Future.value(sessionToken));
-    when(serverConfigure.userAgent)
-        .thenAnswer((_) => Future.value('iOS 11.4/Simulator/1.0.0'));
+    when(() => serverConfigure.baseUri).thenAnswer((_) => serverEndpoint);
+    when(() => serverConfigure.apiToken).thenAnswer((_) async => sessionToken);
+    when(() => serverConfigure.userAgent)
+        .thenAnswer((_) async => 'iOS 11.4/Simulator/1.0.0');
+  });
+
+  setUpAll(() {
+    registerFallbackValue(Uri());
   });
 
   Future<Map<String, String>> _setUpHttpHeader() async {
@@ -39,7 +48,7 @@ void main() {
     };
   }
 
-  Uri _setuHttpRequest(String path, Map<String, String?> queryParameters) {
+  Uri _setUpHttpRequest(String path, Map<String, String?> queryParameters) {
     return Uri(
       scheme: serverEndpoint.scheme,
       host: serverEndpoint.host,
@@ -48,18 +57,14 @@ void main() {
     );
   }
 
-  PostExpectation<Future<http.Response>> _mockPostRequest() {
-    return when(
-      apiClient.post(
-        any,
-        headers: anyNamed('headers'),
-        body: anyNamed('body'),
-      ),
-    );
-  }
-
   void _setUpMockPostHttpClientSuccess200(String? bodyContent) {
-    _mockPostRequest().thenAnswer(
+    when(
+      () => apiClient.post(
+        any(),
+        headers: any(named: 'headers'),
+        body: any(named: 'body'),
+      ),
+    ).thenAnswer(
       (_) async => http.Response(
         bodyContent!,
         200,
@@ -71,10 +76,10 @@ void main() {
   }
 
   group(
-    'GuardianDataSource',
+    GuardianDataSource,
     () {
-      String? bodyContent;
-      GuardianContactEntity? guardian;
+      late String bodyContent;
+      late GuardianContactEntity guardian;
 
       setUp(() {
         bodyContent = JsonUtil.getStringSync(
@@ -88,24 +93,24 @@ void main() {
 
       group('create()', () {
         test(
-          'should perform a POST with X-API-Key',
+          'perform a POST with X-API-Key',
           () async {
             // arrange
             const endPointPath = '/me/guardioes';
             final headers = await _setUpHttpHeader();
-            final request = _setuHttpRequest(endPointPath, {
-              'nome': guardian!.name,
-              'celular': guardian!.mobile,
+            final request = _setUpHttpRequest(endPointPath, {
+              'nome': guardian.name,
+              'celular': guardian.mobile,
             });
             _setUpMockPostHttpClientSuccess200(bodyContent);
             // act
             await dataSource.create(guardian);
             // assert
-            verify(apiClient.post(request, headers: headers));
+            verify(() => apiClient.post(request, headers: headers)).called(1);
           },
         );
         test(
-          'should get a valid ValidField for a successful request',
+          'get a valid ValidField for a successful request',
           () async {
             // arrange
             _setUpMockPostHttpClientSuccess200(bodyContent);
