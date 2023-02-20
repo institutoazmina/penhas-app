49c16d1ab34c1f94b1351ffb8ea39f3ec306f295
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-20 18:44:42 +0100

    test: Controllers migrado para mocktail (#49)

--- test/app/features/quiz/presentation/quiz_controller_test.dart
+++ test/app/features/quiz/presentation/quiz_controller_test.dart
@@ -1,18 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/managers/location_services.dart';
 import 'package:penhas/app/features/quiz/presentation/quiz/quiz_module.dart';
 
 import '../../../../utils/aditional_bind_module.dart';
-import '../../../../utils/helper.mocks.dart';
+
+class MockLocationServices extends Mock implements ILocationServices {}
 
 void main() {
   initModules([
     QuizModule(),
-    AditionalBindModule(binds: [
-      Bind.singleton((i) => MockILocationServices()),
-    ],),
+    AditionalBindModule(
+      binds: [
+        Bind.singleton((i) => MockLocationServices()),
+      ],
+    ),
   ]);
   // QuizController quiz;
   //
