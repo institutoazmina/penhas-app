c07742143877194c0bc49514ac6d6b10f4b6b3cc
Solli Moreira Honorio <shonorio@gmail.com>
2023-02-20 18:44:42 +0100

    test: Controllers migrado para mocktail (#49)

--- test/app/features/mainboard/presentation/mainboard/mainboard_controller_test.dart
+++ test/app/features/mainboard/presentation/mainboard/mainboard_controller_test.dart
@@ -1,18 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/core/network/network_info.dart';
-
 import 'package:penhas/app/features/mainboard/presentation/mainboard/mainboard_module.dart';
 
 import '../../../../../utils/aditional_bind_module.dart';
-import '../../../../../utils/helper.mocks.dart';
+
+class MockNetworkInfo extends Mock implements INetworkInfo {}
 
 void main() {
   initModules([
     AditionalBindModule(
       binds: [
-        Bind.singleton<INetworkInfo>((i) => MockINetworkInfo()),
+        Bind.singleton<INetworkInfo>((i) => MockNetworkInfo()),
       ],
     ),
     MainboardModule(),
