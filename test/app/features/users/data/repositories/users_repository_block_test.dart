d54701838eb027e267bf5d54328c0f812d07d8f9
Edely Gomes <edely.gomes@gmail.com>
2023-03-04 16:06:17 -0300

    Usa mocktail nos testes

--- test/app/features/users/data/repositories/users_repository_block_test.dart
+++ test/app/features/users/data/repositories/users_repository_block_test.dart
@@ -1,20 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/network/api_client.dart';
 import 'package:penhas/app/features/users/data/repositories/users_repository.dart';
 
-import '../../../../../utils/helper.mocks.dart';
+class MockApiProvider extends Mock implements IApiProvider {}
 
 void main() {
-  late final MockIApiProvider apiProvider = MockIApiProvider();
-  late final IUsersRepository sut = UsersRepository(apiProvider: apiProvider);
+  final IApiProvider apiProvider = MockApiProvider();
+  final IUsersRepository sut = UsersRepository(apiProvider: apiProvider);
   group('UsersRepository', () {
     test('should pass clientId on path', () async {
       int clientId = 6543;
       when(
-        apiProvider.post(path: '/profile/$clientId/block'),
+        () => apiProvider.post(path: '/profile/$clientId/block'),
       );
       sut.block(clientId);
-      verify(apiProvider.post(path: '/profile/$clientId/block'));
+      verify(() => apiProvider.post(path: '/profile/$clientId/block'));
     });
   });
 }
