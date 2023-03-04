80a87eb259b645d98265ed5431d403aa7d3790de
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-03-04 11:44:57 -0300

    fix: set barrier color for dialogs (#53)

--- lib/app/core/extension/asuka.dart
+++ lib/app/core/extension/asuka.dart
@@ -2,13 +2,16 @@ import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../shared/design_system/colors.dart';
+
 extension Dialog on IModularNavigator {
-  Future showDialog({
+  Future<T?> showDialog<T>({
     required WidgetBuilder builder,
     bool barrierDismissible = true,
   }) =>
-      Asuka.showDialog(
+      Asuka.showDialog<T>(
         builder: builder,
         barrierDismissible: barrierDismissible,
+        barrierColor: DesignSystemColors.dialogBarrierColor,
       );
 }
