c077781eb64a88991588acfeea34cf66693969d1
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-03-05 11:38:00 -0300

    Testes unitários UserProfileController (#63)
    * Add GetChatChannelTokenUseCase
    
    * Fix imports in UserProfileModule
    
    * Refactor UserProfileController
    
    - handle error
    - use GetChatChannelTokenUseCase instead of IChatChannelRepository
    
    * Rename showError to showSnackBar
    
    * Add unit tests for UserProfileController

--- lib/app/features/users/presentation/user_profile_state.dart
+++ lib/app/features/users/presentation/user_profile_state.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:penhas/app/features/users/domain/entities/user_detail_entity.dart';
+
+import '../domain/entities/user_detail_entity.dart';
 
 part 'user_profile_state.freezed.dart';
 
@@ -8,4 +9,10 @@ class UserProfileState with _$UserProfileState {
   const factory UserProfileState.initial() = _Initial;
   const factory UserProfileState.loaded(UserDetailEntity person) = _Loaded;
   const factory UserProfileState.error(String message) = _ErrorDetails;
+}
+
+@freezed
+class UserProfileReaction with _$UserProfileReaction {
+  factory UserProfileReaction.showSnackBar(String message) =
+      _ReactionShowSnackBar;
 }
---
+++ test/app/features/users/presentation/user_profile_controller_test.dart
@@ -0,0 +1,137 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/error/failures.dart';
+import 'package:penhas/app/features/chat/domain/entities/chat_channel_open_entity.dart';
+import 'package:penhas/app/features/chat/domain/usecases/get_chat_channel_token_usecase.dart';
+import 'package:penhas/app/features/users/domain/entities/user_detail_entity.dart';
+import 'package:penhas/app/features/users/domain/entities/user_detail_profile_entity.dart';
+import 'package:penhas/app/features/users/presentation/user_profile_controller.dart';
+import 'package:penhas/app/features/users/presentation/user_profile_state.dart';
+
+class MockGetChatChannelTokenUseCase extends Mock
+    implements GetChatChannelTokenUseCase {}
+
+class MockModularNavigator extends Mock implements IModularNavigator {}
+
+void main() {
+  group(UserProfileController, () {
+    late UserProfileController controller;
+
+    late GetChatChannelTokenUseCase mockGetChatChannelTokenUseCase;
+    late IModularNavigator mockNavigator;
+    const clientId = 123;
+
+    const user = UserDetailEntity(
+      isMyself: false,
+      profile: UserDetailProfileEntity(
+        nickname: 'nickname',
+        activity: null,
+        avatar: 'profile.svg',
+        clientId: clientId,
+        miniBio: 'a mini bio',
+        skills: 'many skills',
+      ),
+    );
+
+    setUp(() {
+      mockGetChatChannelTokenUseCase = MockGetChatChannelTokenUseCase();
+
+      controller = UserProfileController(
+        person: user,
+        getChatChannelToken: mockGetChatChannelTokenUseCase,
+      );
+
+      mockNavigator = Modular.navigatorDelegate = MockModularNavigator();
+      when(
+        () => mockNavigator.pushReplacementNamed(
+          any(),
+          arguments: any(named: 'arguments'),
+        ),
+      ).thenAnswer((_) async => null);
+    });
+
+    test(
+      'should load initial state',
+      () async {
+        expect(controller.state, const UserProfileState.loaded(user));
+      },
+    );
+    group('openChannel', () {
+      test(
+        'should call GetChatChannelTokenUseCase',
+        () async {
+          // arrange
+          const chat = ChatChannelOpenEntity(token: 'token');
+          when(() => mockGetChatChannelTokenUseCase(clientId))
+              .thenAnswer((_) async => right(chat));
+
+          // act
+          await controller.openChannel();
+
+          // assert
+          verify(() => mockGetChatChannelTokenUseCase(clientId)).called(1);
+        },
+      );
+
+      test(
+        'should navigate to chat when success',
+        () async {
+          // arrange
+          const chatToken = 'chat-token';
+          const chat = ChatChannelOpenEntity(token: chatToken);
+          when(() => mockGetChatChannelTokenUseCase(clientId))
+              .thenAnswer((_) async => right(chat));
+
+          // act
+          await controller.openChannel();
+
+          // assert
+          verify(
+            () => mockNavigator.pushReplacementNamed(
+              '/mainboard/chat/$chatToken',
+              arguments: chat,
+            ),
+          ).called(1);
+        },
+      );
+
+      test(
+        'should set reaction to null when success',
+        () async {
+          // arrange
+          const chat = ChatChannelOpenEntity(token: '');
+          when(() => mockGetChatChannelTokenUseCase(clientId))
+              .thenAnswer((_) async => right(chat));
+
+          // act
+          await controller.openChannel();
+
+          // assert
+          expect(controller.reaction, isNull);
+        },
+      );
+
+      test(
+        'should set reaction to showSnackBar when failure',
+        () async {
+          // arrange
+          when(() => mockGetChatChannelTokenUseCase(clientId))
+              .thenAnswer((_) async => left(ServerFailure()));
+
+          // act
+          await controller.openChannel();
+
+          // assert
+          expect(
+            controller.reaction,
+            UserProfileReaction.showSnackBar(
+              'O servidor está com problema neste momento, tente novamente.',
+            ),
+          );
+        },
+      );
+    });
+  });
+}
