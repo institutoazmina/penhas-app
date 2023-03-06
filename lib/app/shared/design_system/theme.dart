1064a6b93bb242ce44e00a225e50dc1be6ef9088
Pedro Silva <pdx.henrique@yahoo.com>
2023-03-06 08:16:11 -0300

    re-add text theme

--- lib/app/shared/design_system/theme.dart
+++ lib/app/shared/design_system/theme.dart
@@ -4,6 +4,7 @@ abstract class AppTheme {
   static ThemeData of(BuildContext context) {
     final base = Theme.of(context);
     return base.copyWith(
+      textTheme: base.textTheme.apply(fontFamily: 'Lato'),
       bottomSheetTheme: base.bottomSheetTheme.copyWith(
         backgroundColor: Colors.transparent,
       ),
