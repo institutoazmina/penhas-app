9d9e77ea2f02196650a74ef2b166529f67de53c9
Edely Gomes <edely.gomes@gmail.com>
2023-03-06 06:13:03 -0300

    should throw error for invalid client id

--- test/app/features/users/data/repositories/users_repository_block_test.dart
+++ test/app/features/users/data/repositories/users_repository_block_test.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:penhas/app/core/error/exceptions.dart';
 import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/network/api_client.dart';
 import 'package:penhas/app/features/users/data/repositories/users_repository.dart';
@@ -10,11 +9,21 @@ import '../../../../../utils/json_util.dart';
 
 class MockApiProvider extends Mock implements IApiProvider {}
 
+class MockFailure extends Failure {}
+
 void main() {
-  final IApiProvider apiProvider = MockApiProvider();
-  final IUsersRepository sut = UsersRepository(apiProvider: apiProvider);
+  late IUsersRepository sut;
+  late IApiProvider apiProvider;
   String clientId = '6543';
   final parameters = {'cliente_id': clientId};
+
+  setUpAll(() {
+    apiProvider = MockApiProvider();
+    sut = UsersRepository(
+      apiProvider: apiProvider,
+    );
+  });
+
   group('UsersRepository', () {
     test('should send client_id to block', () async {
       when(
@@ -27,6 +36,25 @@ void main() {
       sut.block(clientId);
       verify(() =>
           apiProvider.post(path: '/profile-block', parameters: parameters));
+    });
+
+    test('should throw error for invalid client id', () async {
+      final response = await JsonUtil.getJson(
+        from: 'users/users_block_cliente_id_invalid.json',
+      );
+
+      final error = ServerFailure();
+
+      when(
+        () => apiProvider.post(
+          path: any(named: 'path'),
+          parameters: any(named: 'parameters'),
+        ),
+      ).thenThrow(response);
+
+      final result = await sut.block(clientId);
+
+      expect(result, left(error));
     });
   });
 }
