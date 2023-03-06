a47af10c28a8c4df76431dd0851b28199255bdae
Edely Gomes <edely.gomes@gmail.com>
2023-03-06 05:10:16 -0300

    Mudando escopo das variáveis

--- test/app/features/users/data/repositories/users_repository_block_test.dart
+++ test/app/features/users/data/repositories/users_repository_block_test.dart
@@ -1,17 +1,22 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/error/exceptions.dart';
+import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/network/api_client.dart';
 import 'package:penhas/app/features/users/data/repositories/users_repository.dart';
+
+import '../../../../../utils/json_util.dart';
 
 class MockApiProvider extends Mock implements IApiProvider {}
 
 void main() {
   final IApiProvider apiProvider = MockApiProvider();
   final IUsersRepository sut = UsersRepository(apiProvider: apiProvider);
+  String clientId = '6543';
+  final parameters = {'cliente_id': clientId};
   group('UsersRepository', () {
-    test('should pass clientId on path', () async {
-      String clientId = '6543';
-      final parameters = {'cliente_id': clientId};
+    test('should send client_id to block', () async {
       when(
         () => apiProvider.post(
           path: any(named: 'path'),
