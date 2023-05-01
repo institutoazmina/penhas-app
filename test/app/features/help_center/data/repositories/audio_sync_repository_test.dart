2f3937c625e1b528ebfd5cb141d92dc65cb0d834
Solli Moreira Honorio <shonorio@gmail.com>
2023-05-01 16:03:48 +0200

    test: Teste AudioSyncRepository download

--- test/app/features/help_center/data/repositories/audio_sync_repository_test.dart
+++ test/app/features/help_center/data/repositories/audio_sync_repository_test.dart
@@ -9,6 +9,7 @@ import 'package:penhas/app/core/error/exceptions.dart';
 import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/network/api_client.dart';
 import 'package:penhas/app/features/help_center/data/repositories/audio_sync_repository.dart';
+import 'package:penhas/app/features/help_center/domain/entities/audio_entity.dart';
 
 class MockApiProvider extends Mock implements IApiProvider {}
 
@@ -23,63 +24,122 @@ void main() {
 
   setUpAll(() {
     registerFallbackValue(MultipartFile.fromString('field', 'value'));
+    registerFallbackValue(File('test/assets/audio/silence.aac'));
   });
 
   group(AudioSyncRepository, () {
-    test(
-      'get ValidField for a valid upload',
-      () async {
+    group('upload', () {
+      test(
+        'get ValidField for a valid upload',
+        () async {
+          // arrange
+          final expected = right(
+            const ValidField(message: 'Áudio recebido com sucesso!'),
+          );
+          final audio = AudioData(
+            sequence: '1',
+            createdAt: '1',
+            eventId: '15dba431-b9c9-4983-af75-9f08c3070c15',
+            media: File('test/assets/audio/silence.aac'),
+          );
+          const bodyMessage =
+              '{"message":"Áudio recebido com sucesso!","success":1,"data":{"id":1234}}';
+          when(
+            () => apiProvider.upload(
+              path: any(named: 'path'),
+              file: any(named: 'file'),
+              fields: any(named: 'fields'),
+            ),
+          ).thenAnswer((_) async => bodyMessage);
+          // act
+          final received = await sut.upload(audio);
+          // assert
+          expect(expected, received);
+        },
+      );
+      test(
+        'return Failure when get Exception',
+        () async {
+          // arrange
+          when(
+            () => apiProvider.upload(
+              path: any(named: 'path'),
+              file: any(named: 'file'),
+              fields: any(named: 'fields'),
+            ),
+          ).thenThrow(ApiProviderSessionError());
+          final audio = AudioData(
+            createdAt: '1',
+            sequence: '1',
+            eventId: '15dba431-b9c9-4983-af75-9f08c3070c15',
+            media: File('test/assets/audio/silence.aac'),
+          );
+
+          final expected = left(ServerSideSessionFailed());
+          // act
+          final received = await sut.upload(audio);
+          // assert
+          expect(expected, received);
+        },
+      );
+    });
+
+    group('download', () {
+      test(
+        'get ValidField for a valid download',
+        () async {
+          // arrange
+          final expected = right(
+            const ValidField(),
+          );
+          final audio = AudioEntity(
+            id: '123',
+            audioDuration: '1s',
+            createdAt: DateTime(2021, 1, 1),
+            canPlay: true,
+            isRequested: true,
+            isRequestGranted: true,
+          );
+          final media = File('test/assets/audio/silence.aac');
+          const bodyMessage =
+              '{"message":"Áudio recebido com sucesso!","success":1,"data":{"id":1234}}';
+          when(
+            () => apiProvider.download(
+              path: any(named: 'path'),
+              file: any(named: 'file'),
+              fields: any(named: 'fields'),
+            ),
+          ).thenAnswer((_) async => bodyMessage);
+          // act
+          final received = await sut.download(audio, media);
+          // assert
+          expect(expected, received);
+        },
+      );
+      test('return Failure whe get Exception', () async {
         // arrange
-        final expected = right(
-          const ValidField(message: 'Áudio recebido com sucesso!'),
+        final audio = AudioEntity(
+          id: '123',
+          audioDuration: '1s',
+          createdAt: DateTime(2021, 1, 1),
+          canPlay: true,
+          isRequested: true,
+          isRequestGranted: true,
         );
-        final audio = AudioData(
-          sequence: '1',
-          createdAt: '1',
-          eventId: '15dba431-b9c9-4983-af75-9f08c3070c15',
-          media: File(
-            'test/assets/audio/silence.aac',
-          ),
-        );
-        const bodyMessage =
-            '{"message":"Áudio recebido com sucesso!","success":1,"data":{"id":1234}}';
+        final media = File('test/assets/audio/silence.aac');
         when(
-          () => apiProvider.upload(
-            path: any(named: 'path'),
-            file: any(named: 'file'),
-            fields: any(named: 'fields'),
-          ),
-        ).thenAnswer((_) async => bodyMessage);
-        // act
-        final received = await sut.upload(audio);
-        // assert
-        expect(expected, received);
-      },
-    );
-    test(
-      'return Failure when get Exception',
-      () async {
-        // arrange
-        when(
-          () => apiProvider.upload(
+          () => apiProvider.download(
             path: any(named: 'path'),
             file: any(named: 'file'),
             fields: any(named: 'fields'),
           ),
         ).thenThrow(ApiProviderSessionError());
-        final audio = AudioData(
-          createdAt: '1',
-          sequence: '1',
-          eventId: '15dba431-b9c9-4983-af75-9f08c3070c15',
-          media: File('test/assets/audio/silence.aac'),
-        );
-
         final expected = left(ServerSideSessionFailed());
         // act
-        final received = await sut.upload(audio);
+        final received = await sut.download(audio, media);
         // assert
         expect(expected, received);
-      },
-    );
+      });
+    });
   });
 }
