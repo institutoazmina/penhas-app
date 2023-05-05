afbae342121be270cd1a7a91bda1945a35179233
Edely Gomes <edelygomes@gmail.com>
2023-05-05 08:08:16 -0300

    Organizing tests - wip

--- test/app/features/zodiac/domain/usecases/stealth_security_action_test.dart
+++ test/app/features/zodiac/domain/usecases/stealth_security_action_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:penhas/app/core/entities/user_location.dart';
 import 'package:penhas/app/features/help_center/data/repositories/guardian_repository.dart';
 import 'package:penhas/app/features/zodiac/domain/usecases/stealth_security_action.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,49 +8,84 @@ import 'package:penhas/app/core/managers/location_services.dart';
 import 'package:penhas/app/core/managers/audio_record_services.dart';
 import 'package:penhas/app/features/help_center/domain/usecases/security_mode_action_feature.dart';
 
-
 class MockAudioServices extends Mock implements IAudioRecordServices {}
+
 class MockLocationServices extends Mock implements ILocationServices {}
+
 class MockGuardianRepository extends Mock implements IGuardianRepository {}
-class MockFeatureToggle extends Mock implements SecurityModeActionFeature{}
+
+class MockFeatureToggle extends Mock implements SecurityModeActionFeature {}
+
+class MockGeolocator extends Mock implements Geolocator {}
+
 void main() {
   late ILocationServices locationServices;
   late IAudioRecordServices audioRecordServices;
   late IGuardianRepository guardianRepository;
   late SecurityModeActionFeature featureToggle;
-
   group(StealthSecurityAction, () {
     late StealthSecurityAction sut;
-    
+    late UserLocationEntity userLocation;
+
     locationServices = MockLocationServices();
     audioRecordServices = MockAudioServices();
     guardianRepository = MockGuardianRepository();
     featureToggle = MockFeatureToggle();
 
     setUp(() {
+      userLocation = const UserLocationEntity(latitude: 1.0, longitude: -1.0);
       sut = StealthSecurityAction(
           locationService: locationServices,
-          audioServices: audioRecordServices, 
+          audioServices: audioRecordServices,
           guardianRepository: guardianRepository,
           featureToogle: featureToggle);
     });
 
-     test('should record user message', () {
-          // arrange
-          
-          // act
-          
-          // assert
-          
-        });
+/*
+  test('should record user message', () async {
+      // arrange
+      when(() => audioRecordServices.isPermissionGranted())
+            .thenAnswer((_) async => true);
+      // act
+      final matcher = await sut.start();
+      // assert
+      // expect(actual, matcher);
+    });
 
-      test('should send sms to guardian', () {
+    */
 
-          // arrange
+    test('should get user location', () {
+      // arrange
+      final position = Position(
+          latitude: 1.0,
+          longitude: -1.0,
+          accuracy: 1.0,
+          altitude: 1.0,
+          timestamp: DateTime.now(),
+          speed: 1.0,
+          speedAccuracy: 1.0,
+          heading: 1.0);
+      when(() => locationServices.isPermissionGranted())
+          .thenAnswer((_) async => true);
+      when(() => Geolocator.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.high))
+          .thenAnswer((_) async => position);
+      // act
+      // final matcher = await sut.start();
+      // assert
+      // expect(actual, matcher);
+    });
 
-          // act
 
-          // assert
-      });
+  /*
+    test('should send sms to guardian', () {
+      // arrange
+
+      // act
+
+      // assert
+    });
+
+    */
   });
 }
