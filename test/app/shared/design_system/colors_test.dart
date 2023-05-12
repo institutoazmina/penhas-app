7093d43afd750d47ea7d54da4c2bfb47370f78ee
Solli Moreira Honorio <shonorio@gmail.com>
2023-05-12 12:06:35 +0200

    test: Cobertura de teste para o DesignSystemColors

---
+++ test/app/shared/design_system/colors_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:penhas/app/shared/design_system/colors.dart';
+
+void main() {
+  group('DesignSystemColors', () {
+    test('hexColor correctly convert a hex string to a Color', () {
+      const hexColor = '#AABBCC';
+      const hexColorWithoutHash = 'AABBCC';
+      const expectedColor = Color(0xFFAABBCC);
+
+      expect(DesignSystemColors.hexColor(hexColor), expectedColor);
+      expect(DesignSystemColors.hexColor(hexColorWithoutHash), expectedColor);
+    });
+
+    test('hexColor correctly convert a hex string with alpha to a Color', () {
+      const hexColor = '#AABBCCDD';
+      const hexColorWithoutHash = 'AABBCCDD';
+      const expectedColor = Color(0xAABBCCDD);
+
+      expect(DesignSystemColors.hexColor(hexColor), expectedColor);
+      expect(DesignSystemColors.hexColor(hexColorWithoutHash), expectedColor);
+    });
+  });
+}
