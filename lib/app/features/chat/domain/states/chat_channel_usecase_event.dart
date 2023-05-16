03af4279a96df1785da9121fc1a878e7e24cbd88
Solli Moreira Honorio <shonorio@gmail.com>
2023-05-16 23:43:16 +0200

    fix: typo no 'ChatChannelUseCaseEvent.updateMetadata'

--- lib/app/features/chat/domain/states/chat_channel_usecase_event.dart
+++ lib/app/features/chat/domain/states/chat_channel_usecase_event.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:penhas/app/features/chat/domain/entities/chat_channel_message.dart';
-import 'package:penhas/app/features/chat/domain/entities/chat_channel_session_entity.dart';
-import 'package:penhas/app/features/chat/domain/entities/chat_user_entity.dart';
+
+import '../entities/chat_channel_message.dart';
+import '../entities/chat_channel_session_entity.dart';
+import '../entities/chat_user_entity.dart';
 
 part 'chat_channel_usecase_event.freezed.dart';
 
@@ -12,9 +13,9 @@ class ChatChannelUseCaseEvent with _$ChatChannelUseCaseEvent {
   const factory ChatChannelUseCaseEvent.loaded() = _Loaded;
   const factory ChatChannelUseCaseEvent.updateUser(ChatUserEntity user) =
       _UpdateUser;
-  const factory ChatChannelUseCaseEvent.updateMetada(
+  const factory ChatChannelUseCaseEvent.updateMetadata(
     ChatChannelSessionMetadataEntity metadata,
-  ) = _UpdateMetada;
+  ) = _UpdateMetadata;
   const factory ChatChannelUseCaseEvent.updateMessage(
     List<ChatChannelMessage> messages,
   ) = _UpdateMessage;
