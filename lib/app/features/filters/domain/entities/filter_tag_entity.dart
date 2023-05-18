c3857c10b69ffb4bd9e4bbfe0d1214236dfda015
Solli Moreira Honorio <shonorio@gmail.com>
2023-05-18 16:20:10 +0200

    test: Cobertura de teste no FilterTagEntity

--- lib/app/features/filters/domain/entities/filter_tag_entity.dart
+++ lib/app/features/filters/domain/entities/filter_tag_entity.dart
@@ -14,15 +14,16 @@ class FilterTagEntity extends Equatable {
   final bool isSelected;
 
   @override
-  List<Object?> get props => [id, label!, isSelected];
+  List<Object?> get props => [id, label, isSelected];
 
   @override
   bool get stringify => true;
 
   FilterTagEntity copyWith({String? id, String? label, bool? isSelected}) {
     return FilterTagEntity(
-        id: id ?? this.id,
-        label: label ?? this.label,
-        isSelected: isSelected ?? this.isSelected,);
+      id: id ?? this.id,
+      label: label ?? this.label,
+      isSelected: isSelected ?? this.isSelected,
+    );
   }
 }
