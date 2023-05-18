8561afcefdec7c3cb704e799765d25ecc19bdee6
Solli Moreira Honorio <shonorio@gmail.com>
2023-05-18 16:01:39 +0200

    fix: Migrado 'presentation' para a estrutura de diretório correto

--- lib/app/features/filters/presentation/filter_module.dart
+++ lib/app/features/filters/presentation/filter_module.dart
@@ -1,14 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:penhas/app/features/filters/domain/entities/filter_tag_entity.dart';
-import 'package:penhas/app/features/filters/domain/presentation/filter_controller.dart';
-import 'package:penhas/app/features/filters/domain/presentation/filter_page.dart';
+
+import '../domain/entities/filter_tag_entity.dart';
+import '../presentation/filter_page.dart';
+import 'filter_controller.dart';
 
 class FilterModule extends Module {
   @override
   List<Bind> get binds => [
         Bind.factory(
           (i) => FilterController(
-              tags: i.args?.data as List<FilterTagEntity>? ?? [],),
+            tags: i.args?.data as List<FilterTagEntity>? ?? [],
+          ),
         ),
       ];
 
