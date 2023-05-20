f4f14af5d83585276e748b6ca2d213ed7b8f8e2b
Solli Moreira Honorio <shonorio@gmail.com>
2023-05-20 10:15:41 +0200

    chore: Ajustado a criação do _pageController para viabilizar os testes

--- lib/app/features/mainboard/domain/states/mainboard_store.dart
+++ lib/app/features/mainboard/domain/states/mainboard_store.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
-import 'package:penhas/app/core/managers/modules_sevices.dart';
-import 'package:penhas/app/features/chat/domain/usecases/chat_toggle_feature.dart';
-import 'package:penhas/app/features/feed/domain/usecases/feed_toggle_feature.dart';
-import 'package:penhas/app/features/feed/domain/usecases/tweet_toggle_feature.dart';
-import 'package:penhas/app/features/help_center/domain/usecases/security_mode_action_feature.dart';
-import 'package:penhas/app/features/mainboard/domain/states/mainboard_state.dart';
-import 'package:penhas/app/features/support_center/domain/usecases/support_center_toggle_feature.dart';
+
+import '../../../../core/managers/modules_sevices.dart';
+import '../../../chat/domain/usecases/chat_toggle_feature.dart';
+import '../../../feed/domain/usecases/feed_toggle_feature.dart';
+import '../../../feed/domain/usecases/tweet_toggle_feature.dart';
+import '../../../help_center/domain/usecases/security_mode_action_feature.dart';
+import '../../../support_center/domain/usecases/support_center_toggle_feature.dart';
+import 'mainboard_state.dart';
 
 part 'mainboard_store.g.dart';
 
@@ -14,14 +15,20 @@ class MainboardStore extends _MainboardStoreBase with _$MainboardStore {
   MainboardStore({
     required IAppModulesServices modulesServices,
     required MainboardState initialPage,
-  }) : super(modulesServices, initialPage);
+    PageController? pageController,
+  }) : super(modulesServices, initialPage, pageController ?? PageController());
 }
 
 abstract class _MainboardStoreBase with Store {
-  _MainboardStoreBase(this._modulesServices, this._initialPage);
+  _MainboardStoreBase(
+    this._modulesServices,
+    this._initialPage,
+    this._pageController,
+  );
 
   final IAppModulesServices _modulesServices;
   final MainboardState _initialPage;
+  final PageController _pageController;
   late final Future setupProgress = setup();
 
   late final PageController pageController = _buildPageCtrl();
@@ -35,7 +42,7 @@ abstract class _MainboardStoreBase with Store {
 
   PageController _buildPageCtrl() {
     setupProgress.then((value) => null); // initialize when ctrl is attached
-    return PageController();
+    return _pageController;
   }
 
   @action
