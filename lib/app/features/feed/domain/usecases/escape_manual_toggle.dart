a925686b6619b6dcb108c10bb9ca01b1c3e00f89
Pedro Silva <pdx.henrique@gmail.com>
2023-06-05 02:49:24 -0300

    update unit tests

--- lib/app/features/feed/domain/usecases/escape_manual_toggle.dart
+++ lib/app/features/feed/domain/usecases/escape_manual_toggle.dart
@@ -6,13 +6,13 @@ class EscapeManualToggleFeature {
   }) : _modulesServices = modulesServices;
 
   final IAppModulesServices _modulesServices;
-  static String featureCode = 'mf';
+  static const String featureCode = 'mf';
 
   Future<bool> get isEnabled => _isEnabled();
 
   Future<bool> _isEnabled() async {
     try {
-      return await _modulesServices.feature(name: featureCode) != null;
+      return await _modulesServices.isEnabled(featureCode);
     } catch (_) {
       return false;
     }
