54e00b4c512dc482624cc177b2ea5ed78962b2de
Edely Gomes <edelygomes@gmail.com>
2023-06-06 05:00:39 -0300

    Wip - AudiosPage Widget Test

---
+++ test/app/features/help_center/presentation/audios/audios_page_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:penhas/app/features/help_center/presentation/audios/audios_page.dart';
+
+void main() {
+  // testWidgets('Audios Page', (tester) async {
+  //   await tester.pumpWidget(const AudiosPage());
+  // });
+}
