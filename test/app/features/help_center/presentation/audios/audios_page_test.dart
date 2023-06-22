00446a9411250c7466d40d9cd51d39b8ffd747e2
Edely Gomes <edelygomes@gmail.com>
2023-06-22 03:00:21 -0300

    Test Audios Page With State Loaded

--- test/app/features/help_center/presentation/audios/audios_page_test.dart
+++ test/app/features/help_center/presentation/audios/audios_page_test.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/features/help_center/domain/entities/audio_entity.dart';
+import 'package:penhas/app/features/help_center/domain/entities/audio_play_tile_entity.dart';
 import 'package:penhas/app/features/help_center/domain/states/audios_state.dart';
 import 'package:penhas/app/features/help_center/presentation/audios/audios_controller.dart';
 import 'package:penhas/app/features/help_center/presentation/audios/audios_page.dart';
@@ -40,7 +43,45 @@ void main() {
 
     // assert
     expect(appBar, findsOneWidget);
+  });
 
+  testWidgets('Audios Page With State Loaded', (tester) async {
+    
+    // arrange
+
+    initializeDateFormatting();
+    AudioEntity audioReference = AudioEntity(
+      audioDuration: '123456',
+      canPlay: true,
+      createdAt: DateTime(2023, 2, 3, 14, 44),
+      id: '123',
+      isRequestGranted: true,
+      isRequested: true,
+    );
+
+    List<AudioPlayTileEntity> audios = [
+      AudioPlayTileEntity(
+        audio: audioReference,
+        description: 'Você tem 1 áudio gravado',
+        onPlayAudio: (audioReference) {},
+        onActionSheet: (audioReference) {},
+      )
+    ];
+
+    when(() => mockController.currentState)
+        .thenReturn(AudiosState.loaded(audios, 'Message'));
+
+    final message = find.text('Você tem 1 áudio gravado');
+
+    // act
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: AudiosPage(),
+      ),
+    );
+
+    // assert
+    expect(message, findsOneWidget);
   });
 }
 
