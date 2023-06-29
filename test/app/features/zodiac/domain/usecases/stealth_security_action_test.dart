98141c3001ebec9d4bbbec0734a5cf7a25b2dba3
Edely Gomes <edelygomes@gmail.com>
2023-06-29 20:40:05 -0300

    Ajusta mocks para executar teste

--- test/app/features/zodiac/domain/usecases/stealth_security_action_test.dart
+++ test/app/features/zodiac/domain/usecases/stealth_security_action_test.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:penhas/app/core/entities/user_location.dart';
+import 'package:penhas/app/core/states/location_permission_state.dart';
 import 'package:penhas/app/features/help_center/data/models/alert_model.dart';
 import 'package:penhas/app/features/help_center/data/repositories/guardian_repository.dart';
 import 'package:penhas/app/features/help_center/domain/entities/audio_record_duration_entity.dart';
@@ -11,10 +11,11 @@ import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/core/managers/location_services.dart';
 import 'package:penhas/app/core/managers/audio_record_services.dart';
 import 'package:penhas/app/features/help_center/domain/usecases/security_mode_action_feature.dart';
-import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:penhas/app/shared/widgets/request_location_permission_content_widget.dart';
 
 class MockAudioServices extends Mock implements IAudioRecordServices {}
+
+class MockLocationServices extends Mock implements ILocationServices {}
 
 class MockGuardianRepository extends Mock implements IGuardianRepository {}
 
@@ -32,9 +33,8 @@ void main() {
   group(StealthSecurityAction, () {
     setUp(() {
       registerFallbackValue(UserLocationEntityFake());
-      GeolocatorPlatform.instance = MockGeolocatorPlatform();
-      PermissionHandlerPlatform.instance = MockPermissionHandlerPlatform();
-      locationServices = LocationServices();
+      registerFallbackValue(const RequestLocationPermissionContentWidget());
+      locationServices = MockLocationServices();
       audioRecordServices = MockAudioServices();
       guardianRepository = MockGuardianRepository();
       featureToggle = MockFeatureToggle();
@@ -48,15 +48,16 @@ void main() {
 
     test('should get user location', () async {
       // arrange
-      final mockPermissionHandlerPlatform = PermissionHandlerPlatform.instance;
       debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
 
-      when((() => mockPermissionHandlerPlatform
-              .checkPermissionStatus(Permission.locationWhenInUse)))
-          .thenAnswer((_) async => PermissionStatus.granted);
-      when((() => mockPermissionHandlerPlatform
-              .checkPermissionStatus(Permission.location)))
-          .thenAnswer((_) async => PermissionStatus.granted);
+      when((() => locationServices.requestPermission(
+              title: any(named: 'title'),
+              description: any(named: 'description'))))
+          .thenAnswer((_) async => const LocationPermissionState.granted());
+      when((() => locationServices.isPermissionGranted()))
+          .thenAnswer((_) async => true);
+      when((() => locationServices.currentLocation()))
+          .thenAnswer((_) async => right(const UserLocationEntity()));
       when(() => guardianRepository.alert(any()))
           .thenAnswer((_) async => right(const AlertModel(
                 title: 'Alerta enviado!',
@@ -65,42 +66,16 @@ void main() {
               )));
       when(() => featureToggle.audioDuration)
           .thenAnswer((_) async => const AudioRecordDurationEntity(3, 4));
-      when(() => audioRecordServices.start()).thenAnswer((_) async => {});
+      when(() => audioRecordServices.start()).thenAnswer((_) async {});
+      when(() => audioRecordServices.rotate()).thenAnswer((_) async {});
+
+      expectLater(sut.isRunning, emits(true));
 
       // act
       await sut.start();
 
       // assert
-      // expect(actual, matcher);
-      // verify(sut.getCurrentLocation).called(1);
+      verify(locationServices.currentLocation).called(1);
     });
   });
 }
-
-class MockGeolocatorPlatform extends Mock
-    with
-        // ignore: prefer_mixin
-        MockPlatformInterfaceMixin
-    implements
-        GeolocatorPlatform {
-  @override
-  Future<Position> getCurrentPosition(
-      {LocationSettings? locationSettings}) async {
-    return Position(
-        longitude: -0.01,
-        latitude: .01,
-        timestamp: DateTime(2023, 2, 3, 9, 20),
-        accuracy: 0.1,
-        altitude: 100.01,
-        heading: 1.1,
-        speed: 12.0,
-        speedAccuracy: 0.0);
-  }
-}
-
-class MockPermissionHandlerPlatform extends Mock
-    with
-        // ignore: prefer_mixin
-        MockPlatformInterfaceMixin
-    implements
-        PermissionHandlerPlatform {}
