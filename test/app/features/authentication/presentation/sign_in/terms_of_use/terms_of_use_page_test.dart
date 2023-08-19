5220e54197e31161dad68147a56ecb7073ba1bb4
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-19 11:14:53 +0200

    test: Cobertura de golden test para o Termo de Uso

---
+++ test/app/features/authentication/presentation/sign_in/terms_of_use/terms_of_use_page_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/features/authentication/presentation/sign_in/terms_of_use/terms_of_use_page.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
+
+import '../../../../../../utils/golden_tests.dart';
+import '../tools/webview_mocks.dart';
+
+void main() {
+  late MockWebViewPlatform mockWebViewPlatform;
+  late MockWebViewPlatformController mockWebViewPlatformController;
+  late MockWebViewCookieManagerPlatform mockWebViewCookieManagerPlatform;
+
+  setUpAll(() {
+    registerFallbackValue(FakeBuildContext());
+    registerFallbackValue(FakeCreationParams());
+    registerFallbackValue(FakeWebViewPlatformCallbacksHandler());
+    registerFallbackValue(FakeJavascriptChannelRegistry());
+    registerFallbackValue(FakeWebSettings());
+  });
+
+  setUp(() {
+    // configure webview mock
+    mockWebViewPlatformController = MockWebViewPlatformController();
+    mockWebViewPlatform = MockWebViewPlatform();
+    mockWebViewCookieManagerPlatform = MockWebViewCookieManagerPlatform();
+    when(() => mockWebViewPlatform.build(
+          context: any(named: 'context'),
+          creationParams: any(named: 'creationParams'),
+          webViewPlatformCallbacksHandler:
+              any(named: 'webViewPlatformCallbacksHandler'),
+          javascriptChannelRegistry: any(named: 'javascriptChannelRegistry'),
+          onWebViewPlatformCreated: any(named: 'onWebViewPlatformCreated'),
+          gestureRecognizers: any(named: 'gestureRecognizers'),
+        )).thenAnswer((Invocation invocation) {
+      final WebViewPlatformCreatedCallback onWebViewPlatformCreated =
+          invocation.namedArguments[const Symbol('onWebViewPlatformCreated')]
+              as WebViewPlatformCreatedCallback;
+      return TestPlatformWebView(
+        mockWebViewPlatformController: mockWebViewPlatformController,
+        onWebViewPlatformCreated: onWebViewPlatformCreated,
+      );
+    });
+    when(() => mockWebViewPlatformController.updateSettings(any()))
+        .thenAnswer((i) => Future.value());
+
+    WebView.platform = mockWebViewPlatform;
+    WebViewCookieManagerPlatform.instance = mockWebViewCookieManagerPlatform;
+  });
+
+  tearDown(() {
+    mockWebViewCookieManagerPlatform.reset();
+  });
+
+  group(TermsOfUsePage, () {
+    group('golden test', () {
+      screenshotTest(
+        'looks as expected',
+        fileName: 'privacy_policy_page',
+        pageBuilder: () => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: TermsOfUsePage(baseUrl: Uri()),
+        ),
+      );
+    });
+  });
+}
