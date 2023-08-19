7b373dd726e638a83716b21ad9e2ef16453686ea
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-19 16:36:44 +0200

    test: Cobertura de teste de widget

--- test/app/features/authentication/presentation/deleted_account/deleted_account_page_test.dart
+++ test/app/features/authentication/presentation/deleted_account/deleted_account_page_test.dart
@@ -1,15 +1,20 @@
+import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/app_module.dart';
+import 'package:penhas/app/core/entities/valid_fiel.dart';
+import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/managers/app_configuration.dart';
 import 'package:penhas/app/features/authentication/presentation/deleted_account/deleted_account_page.dart';
 import 'package:penhas/app/features/main_menu/domain/repositories/user_profile_repository.dart';
 
 import '../../../../../utils/golden_tests.dart';
 import '../mocks/app_modules_mock.dart';
-import '../mocks/modules_mock.dart';
+
+class MockModularNavigate extends Mock implements IModularNavigator {}
 
 void main() {
   late IModularNavigator modularNavigator;
@@ -30,7 +35,49 @@ void main() {
   });
 
   group(DeletedAccountPage, () {
-    // testWidgets('deleted account page ...', (tester) async {});
+    testWidgets(
+      'show error message when failure to recovery the account',
+      (tester) async {
+        const errorMessage =
+            'O servidor está com problema neste momento, tente novamente.';
+
+        when(
+          () => AppModulesMock.userProfileRepository
+              .reactivate(token: any(named: 'token')),
+        ).thenAnswer((i) async => dartz.left(ServerFailure()));
+        await tester.pumpWidget(_loadPage());
+
+        // Tap the button
+        await tester.tap(find.text('Reativar Conta'));
+        await tester.pump();
+        expect(find.text(errorMessage), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'redirect to successfully recovered account',
+      (tester) async {
+        when(
+          () => AppModulesMock.userProfileRepository
+              .reactivate(token: any(named: 'token')),
+        ).thenAnswer((i) async => dartz.right(const ValidField()));
+        when(
+          () => AppModulesMock.appConfiguration
+              .saveApiToken(token: any(named: 'token')),
+        ).thenAnswer((i) => Future.value());
+        when(
+          () => modularNavigator.pushReplacementNamed(any()),
+        ).thenAnswer((_) => Future.value());
+
+        await tester.pumpWidget(_loadPage());
+
+        // Tap the button
+        await tester.tap(find.text('Reativar Conta'));
+        await tester.pump();
+
+        verify(() => modularNavigator.pushReplacementNamed('/')).called(1);
+      },
+    );
 
     group('golden tests', () {
       screenshotTest(
