f77ce3f9072271642d8c65aa2387d676d52ce652
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-19 18:18:53 +0200

    test: Cobertura em widget test

--- test/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_page_test.dart
+++ test/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_page_test.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
@@ -6,8 +7,11 @@ import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/app_module.dart';
 import 'package:penhas/app/core/managers/local_store.dart';
 import 'package:penhas/app/features/appstate/domain/entities/user_profile_entity.dart';
+import 'package:penhas/app/features/authentication/domain/entities/session_entity.dart';
 import 'package:penhas/app/features/authentication/domain/repositories/i_authentication_repository.dart';
 import 'package:penhas/app/features/authentication/domain/usecases/password_validator.dart';
+import 'package:penhas/app/features/authentication/presentation/shared/login_button.dart';
+import 'package:penhas/app/features/authentication/presentation/shared/password_text_input.dart';
 import 'package:penhas/app/features/authentication/presentation/sign_in/sign_in_module.dart';
 import 'package:penhas/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_page.dart';
 
@@ -15,14 +19,33 @@ import '../../../../../utils/golden_tests.dart';
 import '../mocks/app_modules_mock.dart';
 import '../mocks/authentication_modules_mock.dart';
 
-// required IAuthenticationRepository repository,
-// required LocalStore<UserProfileEntity> userProfileStore,
-// required PasswordValidator passwordValidator,
-
 void main() {
+  late IModularNavigator modularNavigator;
+
   setUp(() {
     AppModulesMock.init();
     AuthenticationModulesMock.init();
+    modularNavigator = MockModularNavigate();
+    Modular.navigatorDelegate = modularNavigator;
+
+    final userProfileEntity = UserProfileEntity(
+      fullName: 'fullName',
+      nickname: 'nickname',
+      email: 'email@testing.com',
+      birthdate: DateTime(1970),
+      genre: 'genre',
+      minibio: 'minibio',
+      race: 'race',
+      avatar: 'avatar',
+      skill: const ['skill1', 'skill2'],
+      stealthModeEnabled: true,
+      anonymousModeEnabled: true,
+      jaFoiVitimaDeViolencia: false,
+    );
+
+    when(() => AppModulesMock.userProfileStore.retrieve()).thenAnswer(
+      (i) async => userProfileEntity,
+    );
 
     initModules([
       AppModule(),
@@ -42,37 +65,124 @@ void main() {
     Modular.removeModule(AppModule());
   });
 
-  testWidgets('sign in anonymous page ...', (tester) async {
-    // TODO: Implement test
-  });
+  group(SignInAnonymousPage, () {
+    testWidgets(
+      'show screen widgets',
+      (tester) async {
+        await tester.pumpWidget(_loadPage());
 
-  group('golden test', () {
-    setUp(() {
-      final userProfileEntity = UserProfileEntity(
-        fullName: 'fullName',
-        nickname: 'nickname',
-        email: 'email@testing.com',
-        birthdate: DateTime(1970),
-        genre: 'genre',
-        minibio: 'minibio',
-        race: 'race',
-        avatar: 'avatar',
-        skill: const ['skill1', 'skill2'],
-        stealthModeEnabled: true,
-        anonymousModeEnabled: true,
-        jaFoiVitimaDeViolencia: false,
-      );
+        // check if necessary widgets are present
+        expect(find.text('Senha'), findsOneWidget);
+        expect(find.text('Entrar'), findsOneWidget);
+        expect(find.text('Esqueci minha senha'), findsOneWidget);
+        expect(find.text('Acessar outra conta'), findsOneWidget);
+      },
+    );
 
-      when(() => AppModulesMock.userProfileStore.retrieve()).thenAnswer(
-        (i) async => userProfileEntity,
+    testWidgets(
+      'invalid password show a message error when tap LoginButton',
+      (WidgetTester tester) async {
+        const errorMessage =
+            'E-mail e senha precisam estarem corretos para continuar.';
+
+        when(() => AuthenticationModulesMock.passwordValidator
+            .validate(any(), any())).thenAnswer((i) => dartz.left(EmptyRule()));
+
+        await tester.pumpWidget(_loadPage());
+        const validPassword = 'myStr0ngP4ssw0rd';
+
+        // Tap the LoginButton
+        expect(find.text(errorMessage), findsNothing);
+        await tester.enterText(find.byType(PassordInputField), validPassword);
+        await tester.tap(find.byType(LoginButton));
+        await tester.pump();
+        expect(find.text(errorMessage), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'success login redirect page',
+      (WidgetTester tester) async {
+        const sessionToken = 'sessionToken';
+        const validPassword = 'myStr0ngP4ssw0rd';
+
+        when(() => AuthenticationModulesMock.passwordValidator.validate(
+            any(), any())).thenAnswer((i) => dartz.right(validPassword));
+        when(
+          () => AuthenticationModulesMock.authenticationRepository
+              .signInWithEmailAndPassword(
+            emailAddress: any(named: 'emailAddress'),
+            password: any(named: 'password'),
+          ),
+        ).thenAnswer(
+          (i) async =>
+              dartz.right(const SessionEntity(sessionToken: sessionToken)),
+        );
+        when(() => AppModulesMock.appStateUseCase.check())
+            .thenAnswer((i) async => dartz.right(FakeAppStateEntity()));
+
+        when(
+          () => modularNavigator.pushReplacementNamed(any(),
+              arguments: any(named: 'arguments')),
+        ).thenAnswer((_) => Future.value());
+
+        await tester.pumpWidget(_loadPage());
+
+        // Tap the LoginButton
+        await tester.enterText(find.byType(PassordInputField), validPassword);
+        await tester.tap(find.byType(LoginButton));
+        await tester.pump();
+
+        verify(() => modularNavigator.pushReplacementNamed('/mainboard'))
+            .called(1);
+      },
+    );
+
+    testWidgets(
+      'reset password redirect page',
+      (WidgetTester tester) async {
+        when(
+          () => modularNavigator.pushNamed(any(),
+              arguments: any(named: 'arguments')),
+        ).thenAnswer((_) => Future.value());
+
+        await tester.pumpWidget(_loadPage());
+
+        // Tap the sign up button
+        await tester.tap(find.text('Esqueci minha senha'));
+        await tester.pump();
+
+        verify(() =>
+                modularNavigator.pushNamed('/authentication/reset_password'))
+            .called(1);
+      },
+    );
+
+    testWidgets(
+      'change account redirect page',
+      (WidgetTester tester) async {
+        when(
+          () => modularNavigator.pushNamed(any(),
+              arguments: any(named: 'arguments')),
+        ).thenAnswer((_) => Future.value());
+
+        await tester.pumpWidget(_loadPage());
+
+        // Tap the sign up button
+        await tester.tap(find.text('Acessar outra conta'));
+        await tester.pump();
+
+        verify(() => modularNavigator.pushNamed('/authentication')).called(1);
+      },
+    );
+
+    group('golden test', () {
+      screenshotTest(
+        'looks as expected',
+        fileName: 'sign_in_anonymous_page',
+        pageBuilder: _loadPage,
       );
     });
-
-    screenshotTest(
-      'looks as expected',
-      fileName: 'sign_in_anonymous_page',
-      pageBuilder: _loadPage,
-    );
   });
 }
 
