e9f4fe7a626dcad53673d1a1bd41781d6a9add5b
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-20 17:15:12 +0200

    test Cobertura pelo Golden test

---
+++ test/app/features/authentication/presentation/reset_password/pages/reset_password_two/reset_password_two_page_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_test/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:penhas/app/features/authentication/presentation/reset_password/pages/reset_password_two/reset_password_two_controller.dart';
+import 'package:penhas/app/features/authentication/presentation/reset_password/pages/reset_password_two/reset_password_two_page.dart';
+import 'package:penhas/app/features/authentication/presentation/shared/user_register_form_field_model.dart';
+import 'package:penhas/app/features/authentication/presentation/sign_in/sign_in_module.dart';
+
+import '../../../../../../../utils/golden_tests.dart';
+import '../../../mocks/authentication_modules_mock.dart';
+
+void main() {
+  late UserRegisterFormFieldModel userRegisterFormField;
+
+  setUp(() {
+    AuthenticationModulesMock.init();
+    userRegisterFormField = UserRegisterFormFieldModel();
+
+    initModule(
+      SignInModule(),
+      replaceBinds: [
+        Bind<ResetPasswordTwoController>(
+          (i) => ResetPasswordTwoController(
+            AuthenticationModulesMock.changePasswordRepository,
+            userRegisterFormField,
+          ),
+        ),
+      ],
+    );
+  });
+
+  tearDown(() {
+    Modular.removeModule(SignInModule());
+  });
+
+  group(ResetPasswordTwoPage, () {
+    group('golden test', () {
+      screenshotTest(
+        'looks as expected',
+        fileName: 'reset_password_page_step_2',
+        pageBuilder: _loadPage,
+      );
+    });
+  });
+}
+
+Widget _loadPage() {
+  return const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ResetPasswordTwoPage(),
+  );
+}
