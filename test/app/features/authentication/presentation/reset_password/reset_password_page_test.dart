2f263a7da06c911f6c5d08f120982286d4db3300
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-20 12:00:32 +0200

    test: Notificado se ocorrer algum problema na validação

--- test/app/features/authentication/presentation/reset_password/reset_password_page_test.dart
+++ test/app/features/authentication/presentation/reset_password/reset_password_page_test.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/features/authentication/domain/repositories/i_reset_password_repository.dart';
 import 'package:penhas/app/features/authentication/presentation/reset_password/reset_password_page.dart';
 import 'package:penhas/app/features/authentication/presentation/shared/single_text_input.dart';
@@ -54,12 +56,8 @@ void main() {
     testWidgets(
       'do not forward for invalid email',
       (tester) async {
-        // when(()=> );
-
-        //
         await tester.pumpWidget(_loadPage());
         await tester.tap(find.text('Próximo'));
-
         await tester.pump();
 
         // Fetching the TextField's
@@ -67,6 +65,27 @@ void main() {
             .widget(find.byKey(const Key('singleTextInput'))) as TextField;
         InputDecoration? decoration = textField.decoration;
         expect(decoration?.errorText, 'Endereço de email inválido');
+      },
+    );
+
+    testWidgets(
+      'show error for authentication validation error',
+      (WidgetTester tester) async {
+        const errorMessage =
+            'O servidor está com problema neste momento, tente novamente.';
+
+        when(() => AuthenticationModulesMock.resetPasswordRepository
+                .request(emailAddress: any(named: 'emailAddress')))
+            .thenAnswer((i) async => dartz.left(ServerFailure()));
+
+        await tester.pumpWidget(_loadPage());
+
+        // Tap the LoginButton
+        expect(find.text(errorMessage), findsNothing);
+        await tester.enterText(find.byType(SingleTextInput), 'valid@email.com');
+        await tester.tap(find.text('Próximo'));
+        await tester.pump();
+        expect(find.text(errorMessage), findsOneWidget);
       },
     );
 
