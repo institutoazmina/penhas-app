d19e5ceaff60cf5ac25f4c73ef88ef22bbf0c5b5
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-26 16:23:03 +0200

    test: Removido código inválido

--- test/app/features/authentication/presentation/reset_password_two/reset_password_two_page_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('ResetPasswordTwoPage has title', (WidgetTester tester) async {
-    //  await tester.pumpWidget(buildTestableWidget(ResetPasswordTwoPage(title: 'ResetPasswordTwo')));
-    //  final titleFinder = find.text('ResetPasswordTwo');
-    //  expect(titleFinder, findsOneWidget);
-  });
-}
