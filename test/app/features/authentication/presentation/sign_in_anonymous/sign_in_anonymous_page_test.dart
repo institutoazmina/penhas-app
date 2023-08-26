0916c09ca196b28b7cfc5957c3703ce607b050ee
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-26 16:22:15 +0200

    refactor: Refatorado para utilizar as ferramentas de teste de widget

--- test/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_page_test.dart
+++ test/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_page_test.dart
@@ -1,20 +1,17 @@
 import 'package:dartz/dartz.dart' as dartz;
-import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:penhas/app/app_module.dart';
 import 'package:penhas/app/features/appstate/domain/entities/user_profile_entity.dart';
 import 'package:penhas/app/features/authentication/domain/entities/session_entity.dart';
 import 'package:penhas/app/features/authentication/domain/usecases/password_validator.dart';
-import 'package:penhas/app/features/authentication/presentation/shared/login_button.dart';
-import 'package:penhas/app/features/authentication/presentation/shared/password_text_input.dart';
 import 'package:penhas/app/features/authentication/presentation/sign_in/sign_in_module.dart';
 import 'package:penhas/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_controller.dart';
 import 'package:penhas/app/features/authentication/presentation/sign_in_anonymous/sign_in_anonymous_page.dart';
 
 import '../../../../../utils/golden_tests.dart';
+import '../../../../../utils/widget_test_steps.dart';
 import '../mocks/app_modules_mock.dart';
 import '../mocks/authentication_modules_mock.dart';
 
@@ -55,41 +52,38 @@ void main() {
 
   tearDown(() {
     Modular.removeModule(SignInModule());
-    Modular.removeModule(AppModule());
   });
 
   group(SignInAnonymousPage, () {
     testWidgets(
-      'show screen widgets',
+      'shows screen widgets',
       (tester) async {
-        await tester.pumpWidget(_loadPage());
+        await theAppIsRunning(tester, const SignInAnonymousPage());
 
         // check if necessary widgets are present
-        expect(find.text('Senha'), findsOneWidget);
-        expect(find.text('Entrar'), findsOneWidget);
-        expect(find.text('Esqueci minha senha'), findsOneWidget);
-        expect(find.text('Acessar outra conta'), findsOneWidget);
+        await iSeePasswordField(text: 'Senha');
+        await iSeeButton(text: 'Entrar');
+        await iSeeButton(text: 'Esqueci minha senha');
+        await iSeeButton(text: 'Acessar outra conta');
       },
     );
 
     testWidgets(
-      'invalid password show a message error when tap LoginButton',
+      'invalid password shows a message error when tapping LoginButton',
       (WidgetTester tester) async {
+        const validPassword = 'myStr0ngP4ssw0rd';
         const errorMessage =
             'E-mail e senha precisam estarem corretos para continuar.';
 
         when(() => AuthenticationModulesMock.passwordValidator
             .validate(any(), any())).thenAnswer((i) => dartz.left(EmptyRule()));
 
-        await tester.pumpWidget(_loadPage());
-        const validPassword = 'myStr0ngP4ssw0rd';
-
-        // Tap the LoginButton
-        expect(find.text(errorMessage), findsNothing);
-        await tester.enterText(find.byType(PassordInputField), validPassword);
-        await tester.tap(find.byType(LoginButton));
-        await tester.pump();
-        expect(find.text(errorMessage), findsOneWidget);
+        await theAppIsRunning(tester, const SignInAnonymousPage());
+        await iDontSeeText(errorMessage);
+        await iEnterIntoPasswordField(tester,
+            text: 'Senha', password: validPassword);
+        await iTapText(tester, text: 'Entrar');
+        await iSeeText(errorMessage);
       },
     );
 
@@ -119,12 +113,10 @@ void main() {
               .pushReplacementNamed(any(), arguments: any(named: 'arguments')),
         ).thenAnswer((_) => Future.value());
 
-        await tester.pumpWidget(_loadPage());
-
-        // Tap the LoginButton
-        await tester.enterText(find.byType(PassordInputField), validPassword);
-        await tester.tap(find.byType(LoginButton));
-        await tester.pump();
+        await theAppIsRunning(tester, const SignInAnonymousPage());
+        await iEnterIntoPasswordField(tester,
+            text: 'Senha', password: validPassword);
+        await iTapText(tester, text: 'Entrar');
 
         verify(() => AppModulesMock.modularNavigator
             .pushReplacementNamed('/mainboard')).called(1);
@@ -132,18 +124,15 @@ void main() {
     );
 
     testWidgets(
-      'reset password redirect page',
+      'reset password button redirect page',
       (WidgetTester tester) async {
         when(
           () => AppModulesMock.modularNavigator
               .pushNamed(any(), arguments: any(named: 'arguments')),
         ).thenAnswer((_) => Future.value());
 
-        await tester.pumpWidget(_loadPage());
-
-        // Tap the sign up button
-        await tester.tap(find.text('Esqueci minha senha'));
-        await tester.pump();
+        await theAppIsRunning(tester, const SignInAnonymousPage());
+        await iTapText(tester, text: 'Esqueci minha senha');
 
         verify(() => AppModulesMock.modularNavigator
             .pushNamed('/authentication/reset_password')).called(1);
@@ -151,18 +140,15 @@ void main() {
     );
 
     testWidgets(
-      'change account redirect page',
+      'change account button redirect page',
       (WidgetTester tester) async {
         when(
           () => AppModulesMock.modularNavigator
               .pushNamed(any(), arguments: any(named: 'arguments')),
         ).thenAnswer((_) => Future.value());
 
-        await tester.pumpWidget(_loadPage());
-
-        // Tap the sign up button
-        await tester.tap(find.text('Acessar outra conta'));
-        await tester.pump();
+        await theAppIsRunning(tester, const SignInAnonymousPage());
+        await iTapText(tester, text: 'Acessar outra conta');
 
         verify(() =>
                 AppModulesMock.modularNavigator.pushNamed('/authentication'))
@@ -174,15 +160,8 @@ void main() {
       screenshotTest(
         'looks as expected',
         fileName: 'sign_in_anonymous_page',
-        pageBuilder: _loadPage,
+        pageBuilder: () => const SignInAnonymousPage(),
       );
     });
   });
-}
-
-Widget _loadPage() {
-  return const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SignInAnonymousPage(),
-  );
 }
