b3adaace734b18bf54080aacbb53a03291d8ce2c
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-27 19:41:58 +0200

    refactor: Refatorado a inicialização da tela para golden test

--- test/utils/golden_test_widget_app.dart
+++ test/utils/golden_test_widget_app.dart
@@ -2,41 +2,42 @@ import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
-import 'golden_test_device_scenario.dart';
-
 class GoldenTestWidgetApp extends StatelessWidget {
   const GoldenTestWidgetApp({
     Key? key,
     required this.device,
     required this.builder,
-    this.embedInMaterialApp = false,
-    this.navigatorKey,
   }) : super(key: key);
 
   final Device device;
   final ValueGetter<Widget> builder;
-  final bool embedInMaterialApp;
-  final GlobalKey? navigatorKey;
 
   @override
   Widget build(BuildContext context) {
-    final content = DefaultAssetBundle(
+    return DefaultAssetBundle(
       bundle: TestAssetBundle(),
-      child: GoldenTestDeviceScenario(
-        device: device,
-        builder: builder,
+      child: GoldenTestScenario(
+        name: device.name,
+        child: ClipRect(
+          child: MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              size: device.size,
+              padding: device.safeArea,
+              platformBrightness: device.brightness,
+              devicePixelRatio: device.devicePixelRatio,
+              textScaleFactor: device.textScale,
+            ),
+            child: SizedBox(
+              height: device.size.height,
+              width: device.size.width,
+              child: MaterialApp(
+                home: builder(),
+                debugShowCheckedModeBanner: false,
+              ),
+            ),
+          ),
+        ),
       ),
     );
-
-    return embedInMaterialApp
-        ? MediaQuery(
-            data: MediaQueryData(size: device.size, padding: device.safeArea),
-            child: MaterialApp(
-              key: navigatorKey,
-              debugShowCheckedModeBanner: false,
-              home: content,
-            ),
-          )
-        : content;
   }
 }
