462f58e52dc05af73be01250dba8b6b7d26c5974
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-27 21:57:22 +0200

    refactor: Utilizando expansão do mocktail

--- test/app/features/authentication/presentation/reset_password/reset_password_page_test.dart
+++ test/app/features/authentication/presentation/reset_password/reset_password_page_test.dart
@@ -1,4 +1,3 @@
-import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -10,6 +9,7 @@ import 'package:penhas/app/features/authentication/presentation/reset_password/r
 import 'package:penhas/app/features/authentication/presentation/sign_in/sign_in_module.dart';
 
 import '../../../../../utils/golden_tests.dart';
+import '../../../../../utils/mocktail_extension.dart';
 import '../../../../../utils/widget_test_steps.dart';
 import '../mocks/app_modules_mock.dart';
 import '../mocks/authentication_modules_mock.dart';
@@ -35,14 +35,17 @@ void main() {
   });
 
   group(ResetPasswordPage, () {
-    testWidgets('shows screen widgets', (tester) async {
-      await theAppIsRunning(tester, const ResetPasswordPage());
-      await iSeeText('Esqueceu a senha?');
-      await iSeeText(
-          'Informe o seu e-mail para receber o código de recuperação de senha.');
-      await iSeeSingleTextInput(text: 'E-mail');
-      await iSeeButton(text: 'Próximo');
-    });
+    testWidgets(
+      'shows screen widgets',
+      (tester) async {
+        await theAppIsRunning(tester, const ResetPasswordPage());
+        await iSeeText('Esqueceu a senha?');
+        await iSeeText(
+            'Informe o seu e-mail para receber o código de recuperação de senha.');
+        await iSeeSingleTextInput(text: 'E-mail');
+        await iSeeButton(text: 'Próximo');
+      },
+    );
     testWidgets(
       'displays error text for invalid email input',
       (tester) async {
@@ -67,14 +70,14 @@ void main() {
     );
 
     testWidgets(
-      'shows error for authentication validation error',
-      (WidgetTester tester) async {
+      'shows an error for the authentication validation error',
+      (tester) async {
         const errorMessage =
             'O servidor está com problema neste momento, tente novamente.';
 
         when(() => AuthenticationModulesMock.resetPasswordRepository
                 .request(emailAddress: any(named: 'emailAddress')))
-            .thenAnswer((i) async => dartz.left(ServerFailure()));
+            .thenFailure((i) => ServerFailure());
 
         await theAppIsRunning(tester, const ResetPasswordPage());
         await iDontSeeText(errorMessage);
@@ -87,11 +90,11 @@ void main() {
 
     testWidgets(
       'forward for valid email',
-      (WidgetTester tester) async {
+      (tester) async {
         when(() => AuthenticationModulesMock.resetPasswordRepository
                 .request(emailAddress: any(named: 'emailAddress')))
-            .thenAnswer(
-                (i) async => dartz.right(FakeResetPasswordResponseEntity()));
+            .thenSuccess((_) => FakeResetPasswordResponseEntity());
+
         when(() => AppModulesMock.modularNavigator
                 .pushNamed(any(), arguments: any(named: 'arguments')))
             .thenAnswer((i) => Future.value());
