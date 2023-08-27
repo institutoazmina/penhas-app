c51bd1a8e9f19bd4450e8eb1c71716f5fcd2d407
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-27 19:48:38 +0200

    chore: Expansão do Either

--- lib/app/core/extension/either.dart
+++ lib/app/core/extension/either.dart
@@ -1,5 +1,9 @@
 import 'package:dartz/dartz.dart';
 
+Either<L, R> success<L, R>(R r) => right(r);
+
+Either<L, R> failure<L, R>(L l) => left(l);
+
 extension EitherExtension<L extends Object, R> on Either<L, R> {
   R get() => fold((l) => throw l, (r) => r);
 }
