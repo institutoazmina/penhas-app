7e273e121c538d36c708a9ca040848600380a50d
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-27 22:06:13 +0200

    refactor: Utilizando expansão do mocktail

--- test/app/features/authentication/presentation/reset_password/pages/reset_password_two/reset_password_two_page_test.dart
+++ test/app/features/authentication/presentation/reset_password/pages/reset_password_two/reset_password_two_page_test.dart
@@ -1,4 +1,3 @@
-import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
@@ -12,6 +11,7 @@ import 'package:penhas/app/features/authentication/presentation/shared/user_regi
 import 'package:penhas/app/features/authentication/presentation/sign_in/sign_in_module.dart';
 
 import '../../../../../../../utils/golden_tests.dart';
+import '../../../../../../../utils/mocktail_extension.dart';
 import '../../../../../../../utils/widget_test_steps.dart';
 import '../../../mocks/app_modules_mock.dart';
 import '../../../mocks/authentication_modules_mock.dart';
@@ -44,16 +44,19 @@ void main() {
   });
 
   group(ResetPasswordTwoPage, () {
-    testWidgets('shows screen widgets', (tester) async {
-      await theAppIsRunning(tester, const ResetPasswordTwoPage());
-      await iSeeText('Verifique seu e-mail');
-      await iSeeText(
-          'Por favor, digite o código de verificação que enviamos para o e-mail de recuperação.');
-      await iSeeWidget(TextFormField, key: tokenInputKey);
-      await iSeeButton(text: 'Próximo');
-    });
     testWidgets(
-      'shows error for empty token when tap button',
+      'shows screen widgets',
+      (tester) async {
+        await theAppIsRunning(tester, const ResetPasswordTwoPage());
+        await iSeeText('Verifique seu e-mail');
+        await iSeeText(
+            'Por favor, digite o código de verificação que enviamos para o e-mail de recuperação.');
+        await iSeeWidget(TextFormField, key: tokenInputKey);
+        await iSeeButton(text: 'Próximo');
+      },
+    );
+    testWidgets(
+      'shows an error for an empty token when tapping the button',
       (tester) async {
         const errorMessage = 'Precisa digitar o código enviado';
 
@@ -65,7 +68,7 @@ void main() {
     );
 
     testWidgets(
-      'shows error for server side error',
+      'shows an error for server-side error',
       (tester) async {
         const errorMessage =
             'O servidor está com problema neste momento, tente novamente.';
@@ -75,7 +78,7 @@ void main() {
             emailAddress: any(named: 'emailAddress'),
             resetToken: any(named: 'resetToken'),
           ),
-        ).thenAnswer((i) async => dartz.left(ServerFailure()));
+        ).thenFailure((_) => ServerFailure());
 
         await theAppIsRunning(tester, const ResetPasswordTwoPage());
         await iDontSeeText(errorMessage);
@@ -87,14 +90,14 @@ void main() {
     );
 
     testWidgets(
-      'forward to next step for valid token',
+      'forward to the next step for a valid token',
       (tester) async {
         when(
           () => AuthenticationModulesMock.changePasswordRepository.validToken(
             emailAddress: any(named: 'emailAddress'),
             resetToken: any(named: 'resetToken'),
           ),
-        ).thenAnswer((i) async => dartz.right(const ValidField()));
+        ).thenSuccess((_) => const ValidField());
 
         when(
           () => AppModulesMock.modularNavigator
