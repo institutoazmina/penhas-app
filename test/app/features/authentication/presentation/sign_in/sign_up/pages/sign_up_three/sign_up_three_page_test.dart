11b12b0540cb23ee8dbb878b42291c4463773c1d
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-28 21:06:45 +0200

    test: Apresenta todos os elementos da tela

--- test/app/features/authentication/presentation/sign_in/sign_up/pages/sign_up_three/sign_up_three_page_test.dart
+++ test/app/features/authentication/presentation/sign_in/sign_up/pages/sign_up_three/sign_up_three_page_test.dart
@@ -7,6 +7,7 @@ import 'package:penhas/app/features/authentication/presentation/sign_in/sign_up/
 import 'package:penhas/app/features/authentication/presentation/sign_in/sign_up/pages/sign_up_three/sign_up_three_page.dart';
 
 import '../../../../../../../../utils/golden_tests.dart';
+import '../../../../../../../../utils/widget_test_steps.dart';
 import '../../../../mocks/app_modules_mock.dart';
 import '../../../../mocks/authentication_modules_mock.dart';
 
@@ -34,6 +35,21 @@ void main() {
   });
 
   group(SignUpThreePage, () {
+    testWidgets(
+      'shows screen widgets',
+      (tester) async {
+        await theAppIsRunning(tester, const SignUpThreePage());
+
+        // check if necessary widgets are present
+        await iSeeText('Crie sua conta');
+        await iSeeText(
+            'Informe um email e defina uma senha segura. A senha precisa ter no mínimo 8 caracteres, com ao menos 1 letra, 1 número e 1 caractere especial.');
+        await iSeeSingleTextInput(text: 'E-mail');
+        await iSeePasswordField(text: 'Senha');
+        await iSeePasswordField(text: 'Confirmação de senha');
+        await iSeeButton(text: 'Cadastrar');
+      },
+    );
     screenshotTest(
       'looks as expected',
       fileName: 'sign_up_step_3_page',
