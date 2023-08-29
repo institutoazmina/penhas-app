c51d488d08cbf9ab94b7e18cdc2809816e504ad2
Solli Moreira Honorio <shonorio@gmail.com>
2023-08-29 22:28:47 +0200

    refactor: Refatorado o teste que estava quebrado com a atualização do Flutter Modular Test

--- test/app/features/escape_manual/escape_manual_module_test.dart
+++ test/app/features/escape_manual/escape_manual_module_test.dart
@@ -2,35 +2,34 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/app_module.dart';
 import 'package:penhas/app/core/network/api_client.dart';
 import 'package:penhas/app/features/escape_manual/escape_manual_module.dart';
 import 'package:penhas/app/features/escape_manual/presentation/escape_manual_controller.dart';
 
-import '../../../utils/aditional_bind_module.dart';
-
 void main() {
   setUp(() {
-    initModules([
-      AditionalBindModule(
-        binds: [
-          Bind.singleton<IApiProvider>((i) => ApiProviderMock()),
-        ],
-      ),
-      EscapeManualModule(),
-    ]);
+    initModules(
+      [AppModule(), EscapeManualModule()],
+      replaceBinds: [
+        Bind<IApiProvider>((i) => ApiProviderMock()),
+      ],
+    );
   });
 
-  test(
-    'should have EscapeManualController instance',
-    () {
-      // act
-      final controller = Modular.get<EscapeManualController>();
+  group(EscapeManualModule, () {
+    test(
+      'should have EscapeManualController instance',
+      () {
+        // act
+        final controller = Modular.get<EscapeManualController>();
 
-      // assert
-      expect(controller, isNotNull);
-    },
-    skip: true,
-  );
+        // assert
+        expect(controller, isNotNull);
+      },
+      skip: true,
+    );
+  });
 }
 
 class ApiProviderMock extends Mock implements IApiProvider {}
