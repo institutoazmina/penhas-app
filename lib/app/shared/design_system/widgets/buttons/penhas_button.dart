03cb8d23a740503ec56824e199de85e8095ef779
Solli Moreira Honorio <shonorio@gmail.com>
2023-09-02 12:10:47 +0200

    docs: Documentação do PenhasButton

--- lib/app/shared/design_system/widgets/buttons/penhas_button.dart
+++ lib/app/shared/design_system/widgets/buttons/penhas_button.dart
@@ -5,7 +5,41 @@ import 'styles/penhas_button_style.dart';
 import 'styles/rounded_button_style.dart';
 import 'styles/text_button_style.dart';
 
+/// Um botão personalizado para ser usado em toda a aplicação.
+///
+/// `PenhasButton` é um widget que estende [ElevatedButton], permitindo uma
+/// fácil customização e consistência de estilo para botões em toda a aplicação.
+/// Este widget permite a aplicação de estilos personalizados através da interface [PenhasButtonStyle].
+///
+/// Exemplo de uso:
+/// ```dart
+/// PenhasButton(
+///   style: TextButtonStyle(),
+///   child: Text('Botão Arredondado'),
+///   onPressed: () { /* Faça algo aqui */ },
+/// )
+/// ```
+///
+/// Pode utilizar os métodos factory para instâncias no estilo desejado.
+///
+/// Exemplo de uso:
+/// ```dart
+/// PenhasButton.roundedButton(
+///   child: Text('Botão Arredondado'),
+///   onPressed: () { /* Faça algo aqui */ },
+/// )
+/// ```
+///
+/// Veja também:
+/// * [PenhasButtonStyle], a interface que define o estilo de botão.
+/// * [FlatButtonStyle], [RoundedButtonStyle], [TextButtonStyle], as implementações de [PenhasButtonStyle].
 class PenhasButton extends ElevatedButton {
+  /// Cria uma nova instância de [PenhasButton].
+  ///
+  /// * [child] é o conteúdo do botão.
+  /// * [onPressed] é a ação a ser executada ao pressionar o botão.
+  /// * [style] é a implementação da interface [PenhasButtonStyle] que define o estilo do botão.
+  /// * [focusNode] é o [FocusNode] opcional para o botão.
   PenhasButton({
     Key? key,
     required VoidCallback? onPressed,
@@ -20,6 +54,7 @@ class PenhasButton extends ElevatedButton {
           focusNode: focusNode,
         );
 
+  /// Cria uma instância de [PenhasButton] com estilo [RoundedButtonStyle] de botão arredondado.
   factory PenhasButton.roundedButton({
     Key? key,
     required Widget? child,
@@ -34,6 +69,7 @@ class PenhasButton extends ElevatedButton {
     );
   }
 
+  /// Cria uma instância de [PenhasButton] com estilo [TextButtonStyle] de botão de texto.
   factory PenhasButton.textButton({
     Key? key,
     required Widget? child,
@@ -48,6 +84,7 @@ class PenhasButton extends ElevatedButton {
     );
   }
 
+  /// Cria uma instância de [PenhasButton] com estilo [FlatButtonStyle] de botão plano (flat).
   factory PenhasButton.flatButton({
     Key? key,
     required Widget? child,
