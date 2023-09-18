3e6320498a9e3810747cf046eab606b353925e8d
Solli Moreira Honorio <shonorio@gmail.com>
2023-09-18 09:27:13 +0200

    SplashPage in authenticated states forward to `authentication` in ServerFailure

--- test/app/features/splash/splash_page_test.dart
+++ test/app/features/splash/splash_page_test.dart
@@ -3,11 +3,13 @@ import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:penhas/app/core/data/authorization_status.dart';
+import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/features/appstate/domain/entities/user_profile_entity.dart';
 import 'package:penhas/app/features/splash/splash_controller.dart';
 import 'package:penhas/app/features/splash/splash_module.dart';
 import 'package:penhas/app/features/splash/splash_page.dart';
 
+import '../../../utils/mocktail_extension.dart';
 import '../../../utils/widget_test_steps.dart';
 import '../authentication/presentation/mocks/app_modules_mock.dart';
 
@@ -51,10 +53,27 @@ void main() {
     SplashPage,
     () {
       testWidgets(
-        'in anonymous states forward `authentication`',
+        'in anonymous states forward to `authentication`',
         (tester) async {
           when(() => AppModulesMock.appConfiguration.authorizationStatus)
               .thenAnswer((i) async => AuthorizationStatus.anonymous);
+          when(() =>
+                  AppModulesMock.modularNavigator.pushReplacementNamed(any()))
+              .thenAnswer((i) => Future.value());
+
+          await theAppIsRunning(tester, const SplashPage());
+          verify(() => AppModulesMock.modularNavigator
+              .pushReplacementNamed('/authentication')).called(1);
+        },
+      );
+
+      testWidgets(
+        'in authenticated states forward to `authentication` in ServerFailure',
+        (tester) async {
+          when(() => AppModulesMock.appConfiguration.authorizationStatus)
+              .thenAnswer((i) async => AuthorizationStatus.authenticated);
+          when(() => AppModulesMock.appStateUseCase.check())
+              .thenFailure((_) => ServerSideSessionFailed());
           when(() =>
                   AppModulesMock.modularNavigator.pushReplacementNamed(any()))
               .thenAnswer((i) => Future.value());
