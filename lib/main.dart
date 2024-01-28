56ee6884fa555ccb699266144ac838eef103f9db
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2024-01-28 17:33:12 -0300

    Refactoring main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:async';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app/app_module.dart';
@@ -14,8 +14,36 @@ import 'app/app_widget.dart';
 import 'app/core/remoteconfig/remote_config.dart';
 import 'firebase_options.dart';
 
-Future main() async {
+void main() {
+  _runGuardedWithCrashlytics(() {
+    runApp(
+      ModularApp(
+        module: AppModule(),
+        child: const AppWidget(),
+      ),
+    );
+  });
+}
+
+void _runGuardedWithCrashlytics(FutureOr<void> Function() callback) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await _initializeFirebase();
+
+  runZonedGuarded(
+    () async {
+      await Future.wait([
+        Hive.initFlutter(),
+        _initRemoteConfig(),
+      ]);
+
+      await callback();
+    },
+    (error, stack) =>
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+  );
+}
+
+Future<void> _initializeFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -24,28 +52,16 @@ Future main() async {
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   if (kDebugMode) {
-    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-    await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(false);
+    await Future.wait([
+      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false),
+      FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(false),
+    ]);
   }
-
-  runZonedGuarded(
-    () async {
-      await Hive.initFlutter();
-      await _initRemoteConfig();
-      runApp(
-        ModularApp(
-          module: AppModule(),
-          child: const AppWidget(),
-        ),
-      );
-    },
-    (error, stack) =>
-        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
-  );
 }
 
 Future<void> _initRemoteConfig() async {
-  RemoteConfigService remoteConfig =
-      RemoteConfigService(remoteConfig: FirebaseRemoteConfig.instance);
+  final remoteConfig = RemoteConfigService(
+    remoteConfig: FirebaseRemoteConfig.instance,
+  );
   await remoteConfig.initialize();
 }
