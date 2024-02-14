67032a1dff2df3fa9370fcfaac766dfd1b8e374b
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2024-02-14 18:29:57 -0300

    Add tests to bottom navigation

---
+++ test/app/features/mainboard/presentation/mainboard/pages/mainboard_button_page_test.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular_test/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:penhas/app/features/mainboard/domain/states/mainboard_state.dart';
+import 'package:penhas/app/features/mainboard/presentation/mainboard/pages/mainboard_button_page.dart';
+
+void main() {
+  group(MainboarButtonPage, () {
+    testWidgets(
+      'should render button with correct label',
+      (tester) async {
+        // arrange
+        final page = MainboardState.helpCenter();
+        final selectedPage = MainboardState.helpCenter();
+
+        // act
+        await tester.pumpWidget(
+          buildTestableWidget(
+            Material(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  MainboarButtonPage(
+                    onSelect: (page) {},
+                    page: page,
+                    selectedPage: selectedPage,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        // assert
+        expect(find.text(page.label), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'should call onSelect when button is pressed',
+      (tester) async {
+        // arrange
+        final onSelect = _MockOnSelectPage();
+        final page = MainboardState.helpCenter();
+        final selectedPage = MainboardState.feed();
+
+        await tester.pumpWidget(
+          buildTestableWidget(
+            Material(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  MainboarButtonPage(
+                    onSelect: onSelect,
+                    page: page,
+                    selectedPage: selectedPage,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
+        // act
+        await tester.tap(find.byType(InkResponse));
+        await tester.pump();
+
+        // assert
+        verify(() => onSelect(page)).called(1);
+      },
+    );
+  });
+}
+
+class _MockOnSelectPage extends Mock implements IOnSelectPage {}
+
+abstract class IOnSelectPage {
+  void call(MainboardState page);
+}
