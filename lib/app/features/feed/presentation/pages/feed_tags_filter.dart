d8fe59aa0a53d0ca76e735e9b4ce78066204b947
roxyroses <sjennifersarah@gmail.com>
2024-12-26 10:37:48 -0300

    substituir raisedbutton por penhasbutton

--- lib/app/features/feed/presentation/pages/feed_tags_filter.dart
+++ lib/app/features/feed/presentation/pages/feed_tags_filter.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../../../shared/design_system/button_shape.dart';
 import '../../../../shared/design_system/colors.dart';
 import '../../../../shared/design_system/text_styles.dart';
+import '../../../../shared/design_system/widgets/buttons/penhas_button.dart';
 
 class FeedTagsFilter extends StatelessWidget {
   const FeedTagsFilter({
@@ -18,8 +18,7 @@ class FeedTagsFilter extends StatelessWidget {
     return SizedBox(
       height: 32.0,
       width: 160.0,
-      child: RaisedButton(
-        elevation: 0.0,
+      child: PenhasButton.roundedOutlined(
         onPressed: () async {
           Modular.to.pushNamed('/mainboard/tags').then(
             (reload) {
@@ -29,8 +28,6 @@ class FeedTagsFilter extends StatelessWidget {
             },
           );
         },
-        color: DesignSystemColors.white,
-        shape: kButtonShapeOutlinePurple,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: const <Widget>[
