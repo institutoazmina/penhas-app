423409e5c8a341a3098c1000e334230b828159b4
Solli Honorio <shonorio@gmail.com>
2025-01-04 17:29:45 -0300

    feat(support-center): adicionar SupportCenterDetailMapWidget para exibir a localização no mapa

--- lib/app/features/support_center/presentation/pages/widget/support_center_detail_map_widget.dart
+++ lib/app/features/support_center/presentation/pages/widget/support_center_detail_map_widget.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import '../../../../shared/design_system/colors.dart';
-import '../../domain/entities/support_center_place_detail_entity.dart';
+import '../../../../../shared/design_system/colors.dart';
+import '../../../domain/entities/support_center_place_detail_entity.dart';
 
-class SupportCenterDetailMap extends StatelessWidget {
-  const SupportCenterDetailMap({
+class SupportCenterDetailMapWidget extends StatelessWidget {
+  const SupportCenterDetailMapWidget({
     Key? key,
     required this.detail,
   }) : super(key: key);
