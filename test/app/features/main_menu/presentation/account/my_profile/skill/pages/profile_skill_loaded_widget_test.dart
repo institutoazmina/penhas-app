10ac4c98ed2414e4bb515228730703ce49e048fd
Solli Honorio <shonorio@gmail.com>
2025-01-10 17:00:41 -0300

    test(main-menu): adicionar teste de screenshot para ProfileSkillLoadedWidget
    - Incluir teste de screenshot para verificação de regressão visual

---
+++ test/app/features/main_menu/presentation/account/my_profile/skill/pages/profile_skill_loaded_widget_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:penhas/app/features/filters/domain/entities/filter_tag_entity.dart';
+import 'package:penhas/app/features/main_menu/presentation/account/my_profile/skill/pages/profile_skill_loaded_widget.dart';
+
+import '../../../../../../../../utils/golden_tests.dart';
+
+void main() {
+  group(ProfileSkillLoadedWidget, () {
+    screenshotTest(
+      'should render correctly',
+      fileName: 'profile_skill_loaded_widget',
+      pageBuilder: () => Scaffold(
+        body: ProfileSkillLoadedWidget(
+          tags: const [
+            FilterTagEntity(
+              id: '1',
+              label: 'Skill 1',
+              isSelected: false,
+            ),
+            FilterTagEntity(
+              id: '2',
+              label: 'Skill 2',
+              isSelected: true,
+            )
+          ],
+          onApplyFilterAction: (List<FilterTagEntity> tags) {},
+          onResetAction: () {},
+        ),
+      ),
+    );
+  });
+}
