d75b047d00d92a726e4957e2dad22561345acc31
Solli Honorio <shonorio@gmail.com>
2025-01-10 16:01:02 -0300

    test(main-menu): adicionar testes de widget e screenshot para CardProfileNamePage

---
+++ test/app/features/main_menu/presentation/account/pages/card_profile_name_page_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:penhas/app/features/main_menu/presentation/account/pages/card_profile_name_page.dart';
+
+import '../../../../../../utils/golden_tests.dart';
+import '../../../../../../utils/widget_test_steps.dart';
+
+void main() {
+  group(CardProfileNamePage, () {
+    testWidgets('CardProfileNamePage shows modal on edit action',
+        (WidgetTester tester) async {
+      await tester.theAppIsRunning(
+        Scaffold(
+          body: CardProfileNamePage(
+            name: 'PenhaS',
+            onChange: (String newName) {},
+          ),
+        ),
+      );
+
+      iSeeText('Apelido');
+      iSeeText('PenhaS');
+      iSeeWidget(IconButton);
+    });
+
+    screenshotTest(
+      'should render correctly',
+      fileName: 'card_profile_name_page',
+      pageBuilder: () => Scaffold(
+        body: CardProfileNamePage(
+          name: 'PenhaS',
+          onChange: (String newName) {},
+        ),
+      ),
+    );
+  });
+}
