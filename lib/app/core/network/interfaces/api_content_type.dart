ef797c075ee038cd0f829fa336f093207488ff65
Solli Honorio <shonorio@gmail.com>
2025-02-20 15:56:30 +0100

    feature: add content type extension for API requests

--- lib/app/core/network/interfaces/api_content_type.dart
+++ lib/app/core/network/interfaces/api_content_type.dart
@@ -2,3 +2,11 @@ enum ApiContentType {
   json,
   formUrlEncoded,
 }
+
+extension ApiContentTypeExtension on ApiContentType {
+  String get value {
+    return this == ApiContentType.json
+        ? 'application/json; charset=utf-8'
+        : 'application/x-www-form-urlencoded; charset=utf-8';
+  }
+}
