2b53ab6114079699e7374be3846684301e659257
Solli Honorio <shonorio@gmail.com>
2025-02-20 17:18:14 +0100

    refactor: simplify QuizModule dependency injection

--- lib/app/features/quiz/quiz_module.dart
+++ lib/app/features/quiz/quiz_module.dart
@@ -1,15 +1,11 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:http/http.dart' as http;
 
 import '../../core/managers/location_services.dart';
 import '../../core/network/api_client.dart';
-import '../../core/network/api_server_configure.dart';
-import '../../core/network/network_info.dart';
 import '../../core/remoteconfig/i_remote_config.dart';
 import '../../shared/navigation/app_navigator.dart';
 import '../appstate/domain/usecases/app_state_usecase.dart';
 import '../help_center/presentation/pages/tutorial/guardian/guardian_tutorial_page.dart';
-import 'data/datasources/quiz_data_source.dart';
 import 'data/repositories/quiz_repository.dart';
 import 'domain/quiz_remote_config.dart';
 import 'domain/repositories/i_quiz_repository.dart';
@@ -58,17 +54,7 @@ class QuizModule extends Module {
           ),
         ),
         Bind.factory<IQuizRepository>(
-          (i) => QuizRepository(
-            dataSource: i.get<IQuizDataSource>(),
-            networkInfo: i.get<INetworkInfo>(),
-          ),
-        ),
-        Bind.factory<IQuizDataSource>(
-          (i) => QuizDataSource(
-            apiProvider: i.get<IApiProvider>(),
-            apiClient: i.get<http.Client>(),
-            serverConfiguration: i.get<IApiServerConfigure>(),
-          ),
+          (i) => QuizRepository(apiProvider: i.get<IApiProvider>()),
         ),
         Bind.factory<ILocationServices>(
           (i) => LocationServices(),
