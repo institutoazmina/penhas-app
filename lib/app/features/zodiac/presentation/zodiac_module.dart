25642963cac0a1b96a6d6937400acd7acf7e32a4
Solli Honorio <shonorio@gmail.com>
2025-02-21 17:48:09 +0100

    refactor: remove GuardianDataSource from module bindings

--- lib/app/features/zodiac/presentation/zodiac_module.dart
+++ lib/app/features/zodiac/presentation/zodiac_module.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:http/http.dart' as http;
 
 import '../../../core/managers/audio_record_services.dart';
 import '../../../core/managers/audio_sync_manager.dart';
 import '../../../core/managers/local_store.dart';
 import '../../../core/managers/location_services.dart';
 import '../../../core/managers/modules_sevices.dart';
-import '../../../core/network/api_server_configure.dart';
-import '../../../core/network/network_info.dart';
+import '../../../core/network/api_client.dart';
 import '../../appstate/domain/entities/user_profile_entity.dart';
-import '../../help_center/data/datasources/guardian_data_source.dart';
 import '../../help_center/data/repositories/guardian_repository.dart';
 import '../../help_center/domain/usecases/security_mode_action_feature.dart';
 import '../domain/usecases/stealth_security_action.dart';
@@ -42,19 +39,10 @@ class ZodiacModule extends WidgetModule {
           ),
         ),
         Bind.factory<IGuardianRepository>(
-          (i) => GuardianRepository(
-            dataSource: i.get<IGuardianDataSource>(),
-            networkInfo: i.get<INetworkInfo>(),
-          ),
+          (i) => GuardianRepository(apiProvider: i.get<IApiProvider>()),
         ),
         Bind.factory<ILocationServices>(
           (i) => LocationServices(),
-        ),
-        Bind.factory<IGuardianDataSource>(
-          (i) => GuardianDataSource(
-            apiClient: i.get<http.Client>(),
-            serverConfiguration: i.get<IApiServerConfigure>(),
-          ),
         ),
         Bind.factory<IAudioRecordServices>(
           (i) => AudioRecordServices(
