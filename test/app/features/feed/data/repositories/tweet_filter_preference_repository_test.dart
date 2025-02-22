070a32dcdb13bcc03a5d46de229359605c28e111
Solli Honorio <shonorio@gmail.com>
2025-02-22 20:27:59 +0100

    refactor: migrate TweetFilterPreferenceRepository to use API provider directly

--- test/app/features/feed/data/repositories/tweet_filter_preference_repository_test.dart
+++ test/app/features/feed/data/repositories/tweet_filter_preference_repository_test.dart
@@ -1,14 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:penhas/app/core/error/exceptions.dart';
-import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/network/network_info.dart';
 import 'package:penhas/app/features/feed/data/datasources/tweet_filter_preference_data_source.dart';
 import 'package:penhas/app/features/feed/data/models/tweet_filter_session_model.dart';
 import 'package:penhas/app/features/feed/data/repositories/tweet_filter_preference_repository.dart';
-import 'package:penhas/app/features/feed/domain/entities/tweet_filter_session_entity.dart';
 
+import '../../../../../utils/api_provider_mock.dart';
 import '../../../../../utils/json_util.dart';
 
 class MockINetworkInfo extends Mock implements INetworkInfo {}
@@ -17,120 +15,45 @@ class MockITweetFilterPreferenceDataSource extends Mock
     implements ITweetFilterPreferenceDataSource {}
 
 void main() {
-  late INetworkInfo networkInfo;
-  late ITweetFilterPreferenceDataSource dataSource;
   late ITweetFilterPreferenceRepository sut;
 
+  final jsonFile = 'feed/retrieve_filters_tags.json';
+
   setUp(() {
-    networkInfo = MockINetworkInfo();
-    dataSource = MockITweetFilterPreferenceDataSource();
+    ApiProviderMock.init();
 
     sut = TweetFilterPreferenceRepository(
-      networkInfo: networkInfo,
-      dataSource: dataSource,
+      apiProvider: ApiProviderMock.apiProvider,
     );
   });
 
+  void _setUpMockHttpClientResponse(String body, {int? statusCode}) {
+    ApiProviderMock.apiClientResponse(body, statusCode ?? 200);
+  }
+
   group(TweetFilterPreferenceRepository, () {
-    late Map<String, dynamic> jsonSession;
-    late TweetFilterSessionModel sessionModel;
-
-    setUp(() async {
-      jsonSession =
-          await JsonUtil.getJson(from: 'feed/retrieve_fiters_tags.json');
-      sessionModel = TweetFilterSessionModel.fromJson(jsonSession);
-      when(() => dataSource.fetch())
-          .thenAnswer((_) => Future.value(sessionModel));
-    });
-    group('retrieve()', () {
-      test('retrieve tweets from a valid session', () async {
+    test(
+      'retrieve filter preferences from a valid session',
+      () async {
         // arrange
-        final TweetFilterSessionEntity? expectedSession = sessionModel;
+        final jsonSession = await JsonUtil.getJson(from: jsonFile);
+        final sessionModel = TweetFilterSessionModel.fromJson(jsonSession);
+        final jsonString = JsonUtil.getStringSync(from: jsonFile);
+        _setUpMockHttpClientResponse(jsonString);
         // act
-        final receivedSession = await sut.retrieve();
+        final received = await sut.retrieve();
         // assert
-        expect(receivedSession, right(expectedSession));
-      });
+        final request = verify(
+          () => ApiProviderMock.httpClient.send(captureAny()),
+        ).captured.single;
 
-      test(
-          'return an InternetConnectionFailure when the network is disconnected',
-          () async {
-        // arrange
-        const apiProviderException = ApiProviderException(
-          bodyContent: {'error': 'expired_jwt'},
-        );
-        when(() => dataSource.fetch()).thenThrow(apiProviderException);
-        when(() => networkInfo.isConnected).thenAnswer((_) async => false);
-        // act
-        final receivedSession = await sut.retrieve();
-        // assert
-        expect(receivedSession, left(InternetConnectionFailure()));
-      });
+        expect(request.url.path, '/filter-tags');
+        expect(request.method, 'GET');
+        expect(request.headers['Content-Type'],
+            'application/x-www-form-urlencoded; charset=utf-8');
 
-      test('return a ServerFailure when the session is invalid', () async {
-        // arrange
-        const apiProviderException = ApiProviderException(
-          bodyContent: {'error': 'expired_jwt'},
-        );
-        when(() => networkInfo.isConnected).thenAnswer((_) async => true);
-        when(() => dataSource.fetch()).thenThrow(apiProviderException);
-        // act
-        final receivedSession = await sut.retrieve();
-        // assert
-        expect(receivedSession, left(ServerSideSessionFailed()));
-      });
-
-      test(
-          'return a ServerSideFormFieldValidationFailure when got server error',
-          () async {
-        // arrange
-        const apiProviderException = ApiProviderException(
-          bodyContent: {
-            'error': 'invalid_form',
-            'field': 'name',
-            'reason': 'invalid',
-            'message': 'Nome inválido',
-          },
-        );
-        when(() => networkInfo.isConnected).thenAnswer((_) async => true);
-        when(() => dataSource.fetch()).thenThrow(apiProviderException);
-        // act
-        final receivedSession = await sut.retrieve();
-        // assert
-        expect(
-          receivedSession,
-          left(
-            ServerSideFormFieldValidationFailure(
-              error: 'invalid_form',
-              field: 'name',
-              reason: 'invalid',
-              message: 'Nome inválido',
-            ),
-          ),
-        );
-      });
-
-      test('return a ServerSideSessionFailed when got server error', () async {
-        // arrange
-        final apiProviderException = ApiProviderSessionError();
-        when(() => networkInfo.isConnected).thenAnswer((_) async => true);
-        when(() => dataSource.fetch()).thenThrow(apiProviderException);
-        // act
-        final receivedSession = await sut.retrieve();
-        // assert
-        expect(receivedSession, left(ServerSideSessionFailed()));
-      });
-
-      test('return a ServerFailure for a not mapped error', () async {
-        // arrange
-        final apiProviderException = NetworkServerException();
-        when(() => networkInfo.isConnected).thenAnswer((_) async => true);
-        when(() => dataSource.fetch()).thenThrow(apiProviderException);
-        // act
-        final receivedSession = await sut.retrieve();
-        // assert
-        expect(receivedSession, left(ServerFailure()));
-      });
-    });
+        expect(received.fold(id, id), sessionModel);
+      },
+    );
   });
 }
