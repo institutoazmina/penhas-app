a9b382b63491280f9c836146583767cd8dbd9ef9
Solli Honorio <shonorio@gmail.com>
2025-02-23 23:32:30 +0100

    refactor: migrate AuthenticationRepository to use API provider directly

--- test/app/features/authentication/data/repositories/authentication_repository_test.dart
+++ test/app/features/authentication/data/repositories/authentication_repository_test.dart
@@ -2,271 +2,212 @@ import 'package:crypt/crypt.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:penhas/app/core/error/exceptions.dart';
 import 'package:penhas/app/core/error/failures.dart';
 import 'package:penhas/app/core/managers/app_configuration.dart';
-import 'package:penhas/app/core/network/network_info.dart';
-import 'package:penhas/app/core/storage/i_local_storage.dart';
-import 'package:penhas/app/features/authentication/data/datasources/authentication_data_source.dart';
+import 'package:penhas/app/core/network/api_server_configure.dart';
 import 'package:penhas/app/features/authentication/data/models/session_model.dart';
 import 'package:penhas/app/features/authentication/data/repositories/authentication_repository.dart';
-import 'package:penhas/app/features/authentication/domain/entities/session_entity.dart';
 import 'package:penhas/app/features/authentication/domain/repositories/i_authentication_repository.dart';
 import 'package:penhas/app/features/authentication/domain/usecases/email_address.dart';
 import 'package:penhas/app/features/authentication/domain/usecases/password_validator.dart';
 import 'package:penhas/app/features/authentication/domain/usecases/sign_in_password.dart';
 
+import '../../../../../utils/api_provider_mock.dart';
 import '../../../../../utils/json_util.dart';
 
 void main() {
   late EmailAddress email;
   late SignInPassword password;
-  late INetworkInfo networkInfo;
   late IAppConfiguration appConfiguration;
-  late MockAuthenticationDataSource dataSource;
+  late IApiServerConfigure serverConfiguration;
   late IAuthenticationRepository repository;
-  late ILocalStorage localStorage;
   late Crypt hash;
 
+  const contentType = 'application/x-www-form-urlencoded; charset=utf-8';
+
   setUp(() {
+    ApiProviderMock.init();
+
     email = EmailAddress('test@g.com');
     password = SignInPassword('_myStrongP4ss@rd', PasswordValidator());
-    networkInfo = MockNetworkInfo();
     appConfiguration = MockAppConfiguration();
-    dataSource = MockAuthenticationDataSource();
-    localStorage = MockLocalStorage();
+    serverConfiguration = MockIApiServerConfigure();
     hash = Crypt.sha256(password.rawValue!, salt: email.rawValue);
 
     repository = AuthenticationRepository(
       appConfiguration: appConfiguration,
-      dataSource: dataSource,
-      networkInfo: networkInfo,
+      apiProvider: ApiProviderMock.apiProvider,
+      serverConfiguration: serverConfiguration,
     );
+
+    when(() => serverConfiguration.userAgent).thenAnswer((_) async => '1.0.0');
+
+    when(() => appConfiguration.saveApiToken(token: any(named: 'token')))
+        .thenAnswer((_) => Future.value());
+
+    when(() => appConfiguration.saveHash(hash: any(named: 'hash')))
+        .thenAnswer((_) => Future.value());
   });
 
-  void _mockSignInSuccessResponseWith({required SessionModel session}) {
-    when(
-      () => dataSource.signInWithEmailAndPassword(
-        emailAddress: email,
-        password: password,
-      ),
-    ).thenAnswer((_) async => session);
-  }
-
-  void _mockSignInErrorWith({required Exception exception}) {
-    when(
-      () => dataSource.signInWithEmailAndPassword(
-        emailAddress: email,
-        password: password,
-      ),
-    ).thenThrow(exception);
+  void _setUpMockHttpClientResponse(String body, {int? statusCode}) {
+    ApiProviderMock.apiClientResponse(body, statusCode ?? 200);
   }
 
   group(AuthenticationRepository, () {
-    group('SigIn', () {
-      Map<String, dynamic> loginSuccessJson;
-      late SessionModel sessionModel;
-      late SessionEntity sessionEntity;
+    test(
+      'signInWithEmailAndPassword() successfully authenticate and save credentials when valid email and password are provided',
+      () async {
+        // arrange
+        final jsonFile = 'authentication/login_success.json';
+        final jsonData = await JsonUtil.getJson(from: jsonFile);
+        final jsonString = JsonUtil.getStringSync(from: jsonFile);
+        final sessionModel = SessionModel.fromJson(jsonData);
+        _setUpMockHttpClientResponse(jsonString);
 
-      setUp(() async {
-        loginSuccessJson =
-            await JsonUtil.getJson(from: 'authentication/login_success.json');
-        sessionModel = SessionModel.fromJson(loginSuccessJson);
-        sessionEntity = sessionModel;
-      });
-
-      group('when device is online', () {
-        setUp(() {
-          when(() => networkInfo.isConnected).thenAnswer((_) async => true);
-        });
-
-        final authenticationMethods = {
-          'signInWithEmailAndPassword': ({
-            required EmailAddress emailAddress,
-            required SignInPassword password,
-          }) =>
-              repository.signInWithEmailAndPassword(
-                emailAddress: email,
-                password: password,
-              ),
-          'signInOffline': ({
-            required EmailAddress emailAddress,
-            required SignInPassword password,
-          }) =>
-              repository.signInOffline(
-                emailAddress: email,
-                password: password,
-              ),
-        };
-
-        for (final authenticationMethod in authenticationMethods.entries) {
-          final methodName = authenticationMethod.key;
-          final authenticate = authenticationMethod.value;
-
-          group(methodName, () {
-            test(
-              'should return valid SessionEntity for valid user/password',
-              () async {
-                // arrange
-                _mockSignInSuccessResponseWith(session: sessionModel);
-                when(() => appConfiguration.saveApiToken(
-                        token: sessionModel.sessionToken))
-                    .thenAnswer((invocation) => Future.value());
-
-                when(() => appConfiguration.saveHash(hash: hash.toString()))
-                    .thenAnswer((invocation) => Future.value());
-
-                // act
-                final result = await authenticate(
-                  emailAddress: email,
-                  password: password,
-                );
-
-                // assert
-                verify(
-                  () => dataSource.signInWithEmailAndPassword(
-                    emailAddress: email,
-                    password: password,
-                  ),
-                ).called(1);
-
-                verify(() => appConfiguration.saveApiToken(
-                    token: sessionModel.sessionToken)).called(1);
-
-                verify(() => appConfiguration.saveHash(hash: hash.toString()))
-                    .called(1);
-
-                expect(result, right(sessionEntity));
-              },
-            );
-
-            test(
-              'should return server failure when the call to remote server is unsuccessful',
-              () async {
-                // arrange
-                _mockSignInErrorWith(exception: NetworkServerException());
-
-                // act
-                final result = await authenticate(
-                  emailAddress: email,
-                  password: password,
-                );
-
-                // assert
-                expect(result, left(ServerFailure()));
-              },
-            );
-
-            test(
-              'should return login failure for unsuccessful user validation',
-              () async {
-                // arrange
-                final bodyContent = await JsonUtil.getJson(
-                  from: 'authentication/login_failure.json',
-                );
-
-                _mockSignInErrorWith(
-                  exception: ApiProviderException(bodyContent: bodyContent),
-                );
-                // act
-                final result = await authenticate(
-                  emailAddress: email,
-                  password: password,
-                );
-                // assert
-                verify(
-                  () => dataSource.signInWithEmailAndPassword(
-                    emailAddress: email,
-                    password: password,
-                  ),
-                ).called(1);
-                expect(
-                  result,
-                  left(
-                    ServerSideFormFieldValidationFailure(
-                      error: 'wrongpassword',
-                      field: 'password',
-                      reason: 'invalid',
-                      message: 'E-mail ou senha inválida.',
-                    ),
-                  ),
-                );
-              },
-            );
-          });
-        }
-      });
-
-      group('device is offline', () {
-        setUp(() {
-          when(() => networkInfo.isConnected).thenAnswer((_) async => false);
-        });
-
-        test(
-          'should login successful',
-          () async {
-            // arrange
-            _mockSignInErrorWith(exception: const ApiProviderException());
-            when(() => appConfiguration.apiToken)
-                .thenAnswer((invocation) async => sessionModel.sessionToken!);
-            when(() => appConfiguration.offlineHash)
-                .thenAnswer((invocation) async => hash.toString());
-            when(
-              () => dataSource.signInWithOfflineHash(
-                sessionToken: any(named: 'sessionToken'),
-              ),
-            ).thenAnswer((invocation) async => sessionModel);
-
-            // act
-            final result = await repository.signInOffline(
-              emailAddress: email,
-              password: password,
-            );
-
-            //assert
-            verify(
-              () => dataSource.signInWithOfflineHash(
-                sessionToken: sessionModel.sessionToken!,
-              ),
-            ).called(1);
-
-            verify(() => networkInfo.isConnected).called(1);
-            expect(result, right(sessionModel));
-          },
+        // act
+        final result = await repository.signInWithEmailAndPassword(
+          emailAddress: email,
+          password: password,
         );
+        // assert
+        final request = verify(
+          () => ApiProviderMock.httpClient.send(captureAny()),
+        ).captured.single;
 
-        test(
-          'cant login offline when hasnt stored token',
-          () async {
-            // arrange
-            _mockSignInErrorWith(exception: const ApiProviderException());
+        expect(request.url.path, '/login');
+        expect(request.method, 'POST');
+        expect(request.url.queryParameters, {
+          'app_version': '1.0.0',
+        });
+        expect(
+            request.body, 'email%3Dtest%40g.com%26senha%3D_myStrongP4ss%40rd');
+        expect(request.headers['Content-Type'], contentType);
+        expect(result.fold(id, id), sessionModel);
+      },
+    );
 
-            when(() => localStorage.get('br.com.penhas.offlineHash'))
-                .thenAnswer((_) async => null);
+    test(
+      'signInWithEmailAndPassword() successfully authenticate but do not save credentials when user has scheduled deletion',
+      () async {
+        // arrange
+        final jsonFile = 'authentication/login_success_deleted_scheduled.json';
+        final jsonData = await JsonUtil.getJson(from: jsonFile);
+        final jsonString = JsonUtil.getStringSync(from: jsonFile);
+        final sessionModel = SessionModel.fromJson(jsonData);
+        _setUpMockHttpClientResponse(jsonString);
 
-            when(() => appConfiguration.apiToken).thenAnswer(
-                (invocation) async => sessionModel.sessionToken as String);
-            // act
-            final result = await repository.signInOffline(
-              emailAddress: email,
-              password: password,
-            );
-
-            //assert
-            verify(() => networkInfo.isConnected).called(2);
-            expect(result, left(InternetConnectionFailure()));
-          },
+        // act
+        final result = await repository.signInWithEmailAndPassword(
+          emailAddress: email,
+          password: password,
         );
-      });
-    });
+        // assert
+        expect(result.fold(id, id), sessionModel);
+        verifyNever(
+            () => appConfiguration.saveApiToken(token: any(named: 'token')));
+        verifyNever(() => appConfiguration.saveHash(hash: any(named: 'hash')));
+      },
+    );
+
+    test(
+      'signInWithEmailAndPassword() returns ServerSideFormFieldValidationFailure when invalid credentials are provided',
+      () async {
+        // arrange
+        final jsonFile = 'authentication/login_failure.json';
+        final jsonData = await JsonUtil.getJson(from: jsonFile);
+        final jsonString = JsonUtil.getStringSync(from: jsonFile);
+        final sessionModel = ServerSideFormFieldValidationFailure(
+          error: jsonData['error'],
+          field: jsonData['field'],
+          reason: jsonData['reason'],
+          message: jsonData['message'],
+        );
+        _setUpMockHttpClientResponse(jsonString, statusCode: 400);
+
+        // act
+        final result = await repository.signInWithEmailAndPassword(
+          emailAddress: email,
+          password: password,
+        );
+        // assert
+        expect(result.fold(id, id), sessionModel);
+      },
+    );
+
+    test(
+      'signInOffline() successfully authenticate when valid credentials are provided and device is online',
+      () async {
+        // arrange
+        final jsonFile = 'authentication/login_success.json';
+        final jsonData = await JsonUtil.getJson(from: jsonFile);
+        final jsonString = JsonUtil.getStringSync(from: jsonFile);
+        final sessionModel = SessionModel.fromJson(jsonData);
+        _setUpMockHttpClientResponse(jsonString);
+
+        // act
+        final result = await repository.signInOffline(
+          emailAddress: email,
+          password: password,
+        );
+        // assert
+        final request = verify(
+          () => ApiProviderMock.httpClient.send(captureAny()),
+        ).captured.single;
+
+        expect(request.url.path, '/login');
+        expect(request.method, 'POST');
+        expect(request.url.queryParameters, {
+          'app_version': '1.0.0',
+        });
+        expect(
+            request.body, 'email%3Dtest%40g.com%26senha%3D_myStrongP4ss%40rd');
+        expect(request.headers['Content-Type'], contentType);
+        expect(result.fold(id, id), sessionModel);
+      },
+    );
+
+    test(
+      'signInOffline() return Failure when device is offline and no previous credentials are stored',
+      () async {
+        // arrange
+        _setUpMockHttpClientResponse('{}', statusCode: 503);
+        when(() => appConfiguration.offlineHash).thenAnswer((_) async => '');
+        when(() => appConfiguration.apiToken).thenAnswer((_) async => '');
+
+        // act
+        final result = await repository.signInOffline(
+          emailAddress: email,
+          password: password,
+        );
+        // assert
+        expect(result.fold(id, id), InternetConnectionFailure());
+      },
+    );
+
+    test(
+      'signInOffline() successfully authenticate when valid credentials are provided and device is offline',
+      () async {
+        // arrange
+        final jsonFile = 'authentication/login_success.json';
+        final jsonData = await JsonUtil.getJson(from: jsonFile);
+        final sessionModel = SessionModel.fromJson(jsonData);
+        _setUpMockHttpClientResponse('{}', statusCode: 503);
+        when(() => appConfiguration.offlineHash)
+            .thenAnswer((_) async => hash.toString());
+        when(() => appConfiguration.apiToken)
+            .thenAnswer((_) async => sessionModel.sessionToken!);
+        // act
+        final result = await repository.signInOffline(
+          emailAddress: email,
+          password: password,
+        );
+        // assert
+        expect(result.fold(id, id), sessionModel);
+      },
+    );
   });
 }
 
-class MockAuthenticationDataSource extends Mock
-    implements IAuthenticationDataSource {}
-
 class MockAppConfiguration extends Mock implements IAppConfiguration {}
 
-class MockNetworkInfo extends Mock implements INetworkInfo {}
-
-class MockLocalStorage extends Mock implements ILocalStorage {}
+class MockIApiServerConfigure extends Mock implements IApiServerConfigure {}
