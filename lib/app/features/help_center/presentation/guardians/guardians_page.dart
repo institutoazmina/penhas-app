a71d413330e866faa7a0d3834dfcc963758fe4c1
Jennifer Sarah Salomão <sjennifersarah@gmail.com>
2025-03-19 23:35:59 -0300

    Update lib/app/features/help_center/presentation/guardians/guardians_page.dart
    Co-authored-by: Solli Moreira Honorio <shonorio@gmail.com>

--- lib/app/features/help_center/presentation/guardians/guardians_page.dart
+++ lib/app/features/help_center/presentation/guardians/guardians_page.dart
@@ -26,7 +26,7 @@ class GuardiansPage extends StatefulWidget {
   GuardiansPageState createState() => GuardiansPageState();
 }
 
-class GuardiansPageState extends State<GuardiansPage> with SnackBarHandler {
+class _GuardiansPageState extends State<GuardiansPage> with SnackBarHandler {
   List<ReactionDisposer>? _disposers;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
