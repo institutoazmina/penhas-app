80ff8106f1a1c387e0637f2e36f082d07df501bc
roxyroses <sjennifersarah@gmail.com>
2025-03-24 18:34:03 -0300

    remover workmanager

--- lib/app/core/managers/impl/background_task_manager.dart
+++ lib/app/core/managers/impl/background_task_manager.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
+import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:workmanager/workmanager.dart';
 
 import '../../../shared/logger/log.dart';
 import '../background_task_manager.dart';
@@ -10,9 +10,11 @@ import '../background_task_registry.dart';
 class BackgroundTaskManager extends IBackgroundTaskManager {
   BackgroundTaskManager({
     IBackgroundTaskRegistry registry = const BackgroundTaskRegistry(),
-    Workmanager? workManager,
-  })  : _registry = registry; // coverage:ignore-line
+    FlutterBackgroundService? service,
+  })  : _registry = registry,
+        _service = service ?? FlutterBackgroundService();
 
+  final FlutterBackgroundService _service;
   final IBackgroundTaskRegistry _registry;
 
   static IBackgroundTaskManager? _instance;
@@ -22,80 +24,53 @@ class BackgroundTaskManager extends IBackgroundTaskManager {
 
   static set instance(IBackgroundTaskManager value) => _instance = value;
 
-  Future<void> initializeService() async {
-  final service = FlutterBackgroundService();
-
-  await service.configure(
-    androidConfiguration: AndroidConfiguration(
-      onStart: onStart,
-      isForegroundMode: true, // Keeps the task running even when app is closed
-      autoStart: true, // Auto-starts the service on app launch
-    ),
-    iosConfiguration: IosConfiguration(
-      onForeground: onStart,
-      onBackground: onIosBackground,
-    ),
-  );
-
-  service.startService();
-}
-
-@pragma('vm:entry-point')
-void onStart(ServiceInstance service) {
-  if (service is AndroidServiceInstance) {
-    service.setAsForegroundService(); // Runs as a foreground service
-  }
-
-  Timer.periodic(const Duration(seconds: 10), (timer) {
-    service.invoke('update'); // Calls a function periodically
-  });
-}
-
-@pragma('vm:entry-point')
-Future<bool> onIosBackground(ServiceInstance service) async {
-  return true;
-}
-
-@pragma('vm:entry-point')
-void onBackgroundTask(ServiceInstance service) async {
-  if (service is AndroidServiceInstance) {
-    service.setAsForegroundService();
-  }
-
-  service.on('startTask').listen((event) async {
-    final task = event?['task'];
-    
-    try {
-      await _runTaskByName(task);
-      service.invoke('taskCompleted', {'status': true});
-    } catch (e, stack) {
-      logError(e, stack);
-      service.invoke('taskCompleted', {'status': false});
-    }
-  });
-}
-
-
   @override
   Future<void> registerDispatcher(Function callbackDispatcher) async {
-   await initializeService();
+    _service.configure(
+      androidConfiguration: AndroidConfiguration(
+        onStart: _onBackgroundServiceStart,
+        isForegroundMode: true,
+        autoStart: true,
+      ),
+      iosConfiguration: IosConfiguration(
+        onForeground: _onBackgroundServiceStart,
+        onBackground: _onIosBackground,
+      ),
+    );
+  }
+
+  static Future<bool> _onIosBackground(ServiceInstance service) async {
+    return true;
+  }
+
+  static void _onBackgroundServiceStart(ServiceInstance service) {
+    if (kDebugMode) {
+      log('Background service started');
+    }
   }
 
   @override
   void schedule(String taskName) {
-    FlutterBackgroundService().invoke(taskName);
-
+    _service.startService();
+    _service.invoke(taskName);
   }
 
   @override
-  void runPendingTasks() async {
-FlutterBackgroundService().invoke('startTask', {'task': 'yourTaskName'});
-
+  void runPendingTasks() {
+    _service.on('executeTask').listen((event) async {
+      final taskName = event?['taskName'] as String?;
+      if (taskName != null) {
+        try {
+          await _runTaskByName(taskName);
+        } catch (e, stack) {
+          logError(e, stack);
+        }
+      }
+    });
   }
 
   Future<void> _runTaskByName(String taskName) async {
     final taskDefinition = _registry.definitionByName(taskName);
-
     final task = taskDefinition.taskProvider();
     await task.execute();
   }
