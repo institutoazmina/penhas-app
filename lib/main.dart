1bafa483d9f1177a7c8f9f2ffb72a8b444314e0f
roxyroses <sjennifersarah@gmail.com>
2025-04-14 17:57:12 -0300

    fix zone mismatch and google map version

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
 import 'app/core/managers/impl/background_task_manager.dart';
 import 'bootstrap/bootstrap.dart';
 import 'bootstrap/impl/background_tasks_runner.dart';
 import 'bootstrap/impl/main_app_runner.dart';
 
 void main() {
-  Bootstrap.instance.withRunner(
-    MainAppRunner(),
-    onBeforeRun: () {
-      BackgroundTaskManager.instance.registerDispatcher(callbackDispatcher);
-    },
-  );
+  BindingBase.debugZoneErrorsAreFatal = true;
+
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Bootstrap.instance.withRunner(
+      MainAppRunner(),
+      onBeforeRun: () {
+        BackgroundTaskManager.instance.registerDispatcher(callbackDispatcher);
+      },
+    );
+  }, (error, stack) {
+    // Tratamento global de erro
+    log('Error not disclosed $error');
+  });
 }
 
 @pragma('vm:entry-point')
