b09c8aa4407b5a4795a9a91f10ae9ff86ff7084d
rchuluc <rchuluc@gmail.com>
2026-03-04 14:18:40 -0300

    refactor(feed): format code for better readability in feed_controller.dart

--- lib/app/features/feed/presentation/feed_controller.dart
+++ lib/app/features/feed/presentation/feed_controller.dart
@@ -121,8 +121,9 @@ abstract class FeedControllerBase with Store, MapFailureMessage {
 
   void _registerDataSource() {
     _streamCache = _useCase.tweetList().listen(
-      (cache) => state = FeedState.loaded(cache.tweets.toList(growable: false)),
-    );
+          (cache) =>
+              state = FeedState.loaded(cache.tweets.toList(growable: false)),
+        );
 
     fetchNextPage();
   }
